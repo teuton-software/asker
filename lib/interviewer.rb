720e2a75d142fe08d3fc7d12f128ddd5cdf3696a
David Vargas <davidvargas.tenerife@gmail.com>
2014-04-27 18:03:29 +0100

    simplyfing

--- lib/interviewer.rb
+++ lib/interviewer.rb
@@ -13,8 +13,8 @@ The main method of Interviewer class is "run"
 
 Interviewer.run do the next actions:
 1) Read the configuration parameter values.
-2) Read XML files from the directories indicated by "inputdirs" values.
-3) Process the contents and definitions from this XML files.
+2) Read HAML/XML files from the directories indicated by "inputdirs" values.
+3) Process the contents and definitions from this HAML/XML files.
 4) Create GIFT questions from this contens and save it into the file 
 indicated by "outputdir/outputname" values.
 
@@ -26,10 +26,12 @@ class Interviewer
 	
 	def run(pArgs={})
 		init pArgs
+		create_log_file
 		load_input_files
 		show_data if @param[:show_mode]!=:none
-		create_output_file
+		create_output_files
 		show_stats
+		close_log_file
 	end
 	
 	def init(pArgs={})
@@ -43,18 +45,15 @@ class Interviewer
 		
 		@param[:category]=@param[:category] || :none
 		@param[:formula_weights]=@param[:formula_weights] || [1,1,1]
-		@param[:inputdirs]=@param[:inputdirs] || 'input/default'
 		@param[:lang]= @param[:lang] || 'es'
-		@param[:outputdir]=@param[:outputdir] || 'output'
-		@param[:process_file]=@param[:process_file] || 'default.xml'
-		if @param[:process_file]=='default.xml' then
-			@param[:outputname]=@param[:outputname] || 'default-gift.txt'
-		else
-			@param[:outputname]=@param[:outputname] || @param[:process_file].gsub('.xml','.txt')
-		end
-		@param[:logname]=@param[:logname] || 'default.log'
-		@param[:lesson_file]=@param[:lesson_file] || 'default-lesson.txt'
-		@param[:lesson_separator]=@param[:lesson_separator] || ' |'
+		@param[:projectname]=@param[:projectname] || 'default'
+		@param[:inputdirs]=@param[:inputdirs] || "input/#{@param[:projectname]}"
+		@param[:process_file]=@param[:process_file] || "#{@param[:projectname]}.haml"
+		@param[:outputdir]=@param[:outputdir] || "output/#{@param[:projectname]}"
+		@param[:outputname]=@param[:outputname] || "gift-#{@param[:projectname]}.txt"
+		@param[:logname]=@param[:logname] || "#{@param[:projectname]}.log"
+		@param[:lesson_file]=@param[:lesson_file] || "docs-#{@param[:projectname]}.txt"
+		@param[:lesson_separator]=@param[:lesson_separator] || ' >'
 		@param[:show_mode]=@param[:show_mode] || :default
 		@param[:verbose]=@param[:verbose] || true
 
@@ -63,29 +62,30 @@ class Interviewer
 		@verbose=@param[:verbose]
 		@logname=@param[:outputdir]+'/'+@param[:logname]
 		@outputname=@param[:outputdir]+'/'+@param[:outputname]
-
+	end
+	
+	def create_log_file
 		#create or reset logfile
 		Dir.mkdir(@param[:outputdir])	if !Dir.exists? @param[:outputdir]
 
 		@logfile=File.open(@logname,'w')
-		@logfile.write("="*32+"\n")
-		@logfile.write("Proyect: TeacherTools\n")
+		@logfile.write("="*40+"\n")
+		@logfile.write("Proyect: TeacherTools Interviewer\n")
 		@logfile.write("File: #{@logname}\n")
 		@logfile.write("Time: "+Time.new.to_s+"\n")
-		@logfile.write("="*32+"\n")
+		@logfile.write("="*40+"\n")
 	end
 
 	def load_input_files
 		verbose "\n[INFO] Loading input data..."
 		
 		inputdirs=@param[:inputdirs].split(',')
-		
 		inputdirs.each do |dirname|
 			if !Dir.exists? dirname then
 				raise "[ERROR] <#{dirname}> directory dosn't exist!"
 			end
 			files=(Dir.new(dirname).entries-[".",".."]).sort
-			filter = files.select { |f| f[-4..-1]==".xml" || f[-5..-1]==".haml" }
+			filter = files.select { |f| f[-4..-1]==".xml" || f[-5..-1]==".haml" } # filter only HAML or XML files
 			verbose "* HAML/XML files from #{dirname}: #{filter.join(', ').to_s} "
 		
 			filter.each do |f|
@@ -120,7 +120,7 @@ class Interviewer
 			end
 		end
 		
-		#find neighbors
+		#find neighbors for every concept
 		@concepts.each_value do |i|
 			@concepts.each_value do |j|
 				if (i.id!=j.id) then
@@ -146,7 +146,7 @@ class Interviewer
 	
 	def show_stats
 		return if @param[:show_mode]==:none
-		puts "[INFO] Showing concept stats..."
+		verbose "[INFO] Showing concept stats..."
 		total_q=0 
 		total_e=0
 		total_c=0
@@ -154,16 +154,16 @@ class Interviewer
 			if c.process?
 				e=c.data[:texts].size
 				c.data[:tables].each { |t| e=e+t.data[:fields].size*t.data[:rows].size }
-				puts "* Concept: name=#{c.name} "+"-"*(30-c.name.size).abs+"(Q=#{c.num.to_s}, E=#{e.to_s}, %=#{(c.num/e*100).to_i})"
+				verbose "* Concept: name=#{c.name} "+"-"*(30-c.name.size).abs+"(Q=#{c.num.to_s}, E=#{e.to_s}, %=#{(c.num/e*100).to_i})"
 				total_q+=c.num
 				total_e+=e
 				total_c+=1
 			end
 		end
-		puts "* TOTAL(#{total_c.to_s}) "+"-"*35+"(Q=#{total_q.to_s}, E=#{total_e.to_s}, %=#{(total_q/total_e*100).to_i})"
+		verbose "* TOTAL(#{total_c.to_s}) "+"-"*35+"(Q=#{total_q.to_s}, E=#{total_e.to_s}, %=#{(total_q/total_e*100).to_i})"
 	end
 	
-	def create_output_file
+	def create_output_files
 		verbose "\n[INFO] Creating output files..."
 
 		lFile=File.new(@outputname,'w')
@@ -184,17 +184,18 @@ class Interviewer
 			end
 			lFile.close
 		end
-		
+	end
+	
+	def close_log_file
 		@logfile.close
 	end
 
 	def init_config_file(pFilename='default')
 		init
-		
-		tags=[ :inputdirs, :lesson_file, :logname, :outputname, :process_file ]		
-		tags.each { |t| @param[t]=@param[t].gsub('default',pFilename) }
-
-		puts YAML::dump(@param)
+		tags=[ :projectname, :inputdirs, :process_file]
+		sample={}
+		tags.each { |t| sample[t]=@param[t].gsub('default',pFilename) }
+		puts YAML::dump(sample)
 	end
 	
 private	
