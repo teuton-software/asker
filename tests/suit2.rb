cc594a3d47d67c93a0f47114fa9f44ed97505206
David Vargas <davidvargas.tenerife@gmail.com>
2016-07-24 16:30:55 +0100

    N

--- tests/suit2.rb
+++ tests/suit2.rb
@@ -3,5 +3,6 @@
 require 'minitest/autorun'
 
 #require_relative 'application_test'
-require_relative 'lang_test'
+#require_relative 'lang_test'
 require_relative 'concept2_test'
+require_relative 'question2_test'
