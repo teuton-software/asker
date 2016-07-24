057dd969be40a40b6dc2feff307bdb0e34c3a9cb
David Vargas <davidvargas.tenerife@gmail.com>
2016-07-24 13:06:10 +0100

    U

---
+++ lib/project_builder.rb
@@ -0,0 +1,62 @@
+# encoding: utf-8
+
+require 'rainbow'
+
+module ProjectBuilder
+
+  def self.create_project(projectname)
+    app=Application.instance
+
+    puts "\n[INFO] Creating project <#{Rainbow(projectname).bright}>"
+    projectdir="projects/#{projectname}"
+    if !Dir.exists? projectdir
+      puts "* Creating directory => #{Rainbow(projectdir).color(:green)}"
+      Dir.mkdir(projectdir)
+    else
+      puts "* Exists directory! => #{Rainbow(projectdir).color(:yellow)}"
+    end
+
+    filename=projectdir+"/config.yaml"
+    if !File.exists? filename
+      puts "* Creating file => #{Rainbow(filename).color(:green)}"
+      f=File.new(filename,'w')
+      f.write("---\n")
+      f.write(":inputdirs: '#{app.inputbasedir}/#{projectname}'\n")
+      f.write(":process_file: '#{projectname}.haml'\n")
+      f.write("\n")
+      f.close
+    else
+      puts "* Exists file! => #{Rainbow(filename).color(:yellow)}"
+    end
+
+    filename=projectdir+"/.gitignore"
+    if !File.exists? filename
+      puts "* Creating file => #{Rainbow(filename).color(:green)}"
+      f=File.new(filename,'w')
+      f.write("*.txt\n*.log\n*.tmp\n")
+      f.close
+    else
+      puts "* Exists file! => #{Rainbow(filename).color(:yellow)}"
+    end
+
+    mapdir="#{app.inputbasedir}/#{projectname}"
+    if !Dir.exists? mapdir
+      puts "* Creating directory => #{Rainbow(mapdir).color(:green)}"
+      Dir.mkdir(mapdir)
+    else
+      puts "* Exists directory! => #{Rainbow(mapdir).color(:yellow)}"
+    end
+
+    filename=mapdir+"/"+projectname+".haml"
+    if !File.exists? filename
+      puts "* Creating file => #{Rainbow(filename).color(:green)}"
+      f=File.new(filename,'w')
+      f.write(DATA.read)
+      f.close
+    else
+      puts "* Exists file! => #{Rainbow(filename).color(:yellow)}"
+    end
+    puts ""
+  end
+
+end
