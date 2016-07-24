a570dd00b55fb9c727664fc69f10734f2b4eab1d
David Vargas <davidvargas.tenerife@gmail.com>
2016-07-24 16:49:40 +0100

    N test

--- tests/question_test.rb
+++ tests/question_test.rb
@@ -11,9 +11,16 @@ class TestLang < Minitest::Test
     q1.comment="my comment 1"
     q1.text="my text 1"
     q1.set_boolean
-    q1.good="my good 1"
+    q1.good=true
 
-    @questions = [ q0 , q1 ]
+    q2 = Question.new
+    q2.name="my name 2"
+    q2.comment="my comment 2"
+    q2.text="my text 2"
+    q2.set_choice
+    q2.good="good 2"
+    q2.bads = [ "bad1", "bad2", "bad3" ]
+    @questions = [ q0 , q1, q2 ]
   end
 
   def test_init
@@ -29,14 +36,14 @@ class TestLang < Minitest::Test
     assert_equal [], q.shorts
   end
 
-  def test_change_and_reset
+  def test_boolean
     q = @questions[1]
 
     assert_equal "my name 1", q.name
     assert_equal "my comment 1", q.comment
     assert_equal "my text 1", q.text
     assert_equal :boolean, q.type
-    assert_equal "my good 1", q.good
+    assert_equal true, q.good
 
     q.reset
 
@@ -48,19 +55,28 @@ class TestLang < Minitest::Test
     assert_equal [], q.bads
     assert_equal [], q.matching
     assert_equal [], q.shorts
-
   end
 
-  def test_set_types
-    q = @questions[0]
-    q.set_boolean
-    assert_equal :boolean, q.type
-    q.set_match
-    assert_equal :match, q.type
-    q.set_short
-    assert_equal :short, q.type
-    q.set_choice
+  def test_choice
+    q = @questions[2]
+
+    assert_equal "my name 2", q.name
+    assert_equal "my comment 2", q.comment
+    assert_equal "my text 2", q.text
     assert_equal :choice, q.type
+    assert_equal "good 2", q.good
+    assert_equal [ "bad1", "bad2", "bad3" ], q.bads
+
+    q.reset
+
+    assert_equal "", q.name
+    assert_equal "", q.comment
+    assert_equal "", q.text
+    assert_equal :choice, q.type
+    assert_equal "", q.good
+    assert_equal [], q.bads
+    assert_equal [], q.matching
+    assert_equal [], q.shorts
   end
 
 end
