fcf6d6fe21a3f0e90b159b28d74b3eda191866df
David Vargas <davidvargas.tenerife@gmail.com>
2016-07-24 12:37:45 +0100

    U spaces

--- lib/concept/lang/text_actions.rb
+++ lib/concept/lang/text_actions.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 
 module TextActions
-	
+
   def text_for(pOption, pText1="",pText2="",pText3="",pText4="",pText5="",pText6="",pText7="")
     text1=pText1
     text2=pText2
@@ -10,60 +10,58 @@ module TextActions
     text5=pText5
     text6=pText6
     text7=pText7
-	
-	#TODO: check if exists pOption before use it
+
+	  # TODO: check if exists pOption before use it
     renderer = ERB.new(@templates[pOption])
     output = renderer.result(binding)
     return output
   end
-	
+
   def text_filter_connectors(pText, pFilter)
-	input_lines=pText.split(".")
-	output_lines=[]
-	output_words=[]
-	input_lines.each_with_index do |line, rowindex| 
-	  row=[]
-	  line.split(" ").each_with_index do |word,colindex|
-		flag=@connectors.include? word.downcase
-	    
-	    # if <word> is a conector and <pFilter>==true Then Choose this <word>
-	    # if <word> isn't a conector and <pFilter>==true and <word>.length>1 Then Choose this <word>
-		if (flag and pFilter) or (!flag and !pFilter and word.length>1) then
-		  output_words<< {:word => word, :row => rowindex, :col => colindex }
-		  row << (output_words.size-1)
-	    else
-		  row << word
-		end
+    input_lines=pText.split(".")
+    output_lines=[]
+    output_words=[]
+    input_lines.each_with_index do |line, rowindex|
+	    row=[]
+      line.split(" ").each_with_index do |word,colindex|
+        flag=@connectors.include? word.downcase
+
+	      # if <word> is a conector and <pFilter>==true Then Choose this <word>
+	      # if <word> isn't a conector and <pFilter>==true and <word>.length>1 Then Choose this <word>
+        if (flag and pFilter) or (!flag and !pFilter and word.length>1) then
+		      output_words<< {:word => word, :row => rowindex, :col => colindex }
+		      row << (output_words.size-1)
+	      else
+		      row << word
+		    end
+	    end
+	    row << "."
+	    output_lines << row
 	  end
-	  row << "."
-	  output_lines << row
-	end		
-	result={}
-	result[:lines]=output_lines
-	result[:words]=output_words
-	return result
+	  result={ :lines => output_lines, :words => output_words }
+	  return result
   end
 
   def text_with_connectors(pText)
-	return text_filter_connectors(pText, false)
+	  return text_filter_connectors(pText, false)
   end
 
   def text_without_connectors(pText)
-	return text_filter_connectors(pText, true)
+	  return text_filter_connectors(pText, true)
   end
-	
+
   def build_text_from_filtered( pStruct, pIndexes)
     lines = pStruct[:lines]
-				
+
     lText=""
     lines.each do |line|
       line.each do |value|
         if value.class==String
-          lText+=" "+value 
+          lText+=" "+value
         elsif value.class==Fixnum
           if pIndexes.include? value then
             lText+=" [#{value.to_s}]"
-          else	
+          else
             lrow = pStruct[:words][value][:row]
             lcol = pStruct[:words][value][:col]
             lword = pStruct[:words][value][:word]
@@ -84,13 +82,13 @@ module TextActions
     t.gsub!(","," ")
     t.gsub!("   "," ")
     t.gsub!("  "," ")
-    return t.split(" ").count    
+    return t.split(" ").count
   end
 
   def do_mistake_to(pText)
     lText=pText.dup
     keys=@mistakes.keys
-    
+
     keys.shuffle!
     keys.each do |key|
       if lText.include? key.to_s then
@@ -100,7 +98,7 @@ module TextActions
          return lText
       end
     end
-    
+
     return lText
   end
 
@@ -116,5 +114,5 @@ module TextActions
     end
     return output
   end
-    
+
 end
