50a1b54296bd74a3cdd9d2a10faa531436483cff
David Vargas <davidvargas.tenerife@gmail.com>
2016-07-24 13:07:08 +0100

    U another class for project building

--- lib/tool/create_actions.rb
+++ lib/tool/create_actions.rb
@@ -2,10 +2,10 @@
 # encoding: utf-8
 
 module CreateActions
-	
+
   def create_output_files
     app=Application.instance
-    
+
     verbose "\n[INFO] Creating output files..."
 
     lFile=File.new(@outputname,'w')
@@ -14,73 +14,18 @@ module CreateActions
     lFile.write("// Create automatically by David Vargas\n")
     lFile.write("\n")
     lFile.write("$CATEGORY: $course$/#{app.category.to_s}\n") if app.category!=:none
-    @concepts.each_value do |c| 
+    @concepts.each_value do |c|
       c.write_questions_to(lFile) if c.process?
     end
     lFile.close
-		
+
     if app.param[:lesson_file]!=:none then
       lFile=File.new(app.outputdir+'/'+app.lesson_file,'w')
-      @concepts.each_value do |c| 
+      @concepts.each_value do |c|
         c.write_lesson_to(lFile) if c.process?
       end
       lFile.close
     end
-  end
-	
-  def create_project(projectname)
-    app=Application.instance
-
-    puts "\n[INFO] Creating project <#{Rainbow(projectname).bright}>"
-    projectdir="projects/#{projectname}"
-    if !Dir.exists? projectdir
-      puts "* Creating directory => #{Rainbow(projectdir).color(:green)}"
-      Dir.mkdir(projectdir)
-    else
-      puts "* Exists directory! => #{Rainbow(projectdir).color(:yellow)}"
-    end
-    
-    filename=projectdir+"/config.yaml"
-    if !File.exists? filename
-      puts "* Creating file => #{Rainbow(filename).color(:green)}"
-      f=File.new(filename,'w')
-      f.write("---\n")
-      f.write(":inputdirs: '#{app.inputbasedir}/#{projectname}'\n")
-      f.write(":process_file: '#{projectname}.haml'\n")
-      f.write("\n")
-      f.close
-    else
-      puts "* Exists file! => #{Rainbow(filename).color(:yellow)}"
-    end
-    
-    filename=projectdir+"/.gitignore"
-    if !File.exists? filename
-      puts "* Creating file => #{Rainbow(filename).color(:green)}"
-      f=File.new(filename,'w')
-      f.write("*.txt\n*.log\n*.tmp\n")
-      f.close
-    else
-      puts "* Exists file! => #{Rainbow(filename).color(:yellow)}"
-    end
-
-    mapdir="#{app.inputbasedir}/#{projectname}"
-    if !Dir.exists? mapdir
-      puts "* Creating directory => #{Rainbow(mapdir).color(:green)}"
-      Dir.mkdir(mapdir)
-    else
-      puts "* Exists directory! => #{Rainbow(mapdir).color(:yellow)}"
-    end
-    
-    filename=mapdir+"/"+projectname+".haml"
-    if !File.exists? filename
-      puts "* Creating file => #{Rainbow(filename).color(:green)}"
-      f=File.new(filename,'w')
-      f.write(DATA.read)
-      f.close
-    else
-      puts "* Exists file! => #{Rainbow(filename).color(:yellow)}"
-    end
-    puts "" 
   end
   
 end
