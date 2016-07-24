df29a3ea6940310f18b4f375128ab68ccf655011
David Vargas <davidvargas.tenerife@gmail.com>
2016-07-24 13:05:55 +0100

    U

--- lib/lang/lang.rb
+++ lib/lang/lang.rb
@@ -3,25 +3,23 @@
 require 'erb'
 require 'yaml'
 require_relative 'text_actions'
+require_relative '../tool'
 
 class Lang
   include TextActions
 
   attr_accessor :lang
 
-  def initialize(lang='en')
-    @lang=lang.to_s
+  def initialize(pLang='en')
+    @lang=pLang.to_s
 
-    d=(__FILE__).split("/")
-    d.delete_at(-1)
-    dirbase=d.join("/")
-
-    filename=File.join(dirbase, "locales", @lang ,"templates.yaml" )
+    dirbase = File.dirname(__FILE__)
+    filename=File.join( dirbase, "locales", @lang ,"templates.yaml" )
     begin
       @templates=YAML::load(File.new(filename))
     rescue Exception => e
-      puts "[ERROR]  Reading YAML file <#{filename}>"
-      puts "[ADVISE] Perhaps you use apostrophe into string without \\ character"
+      Tool.instance.verbose "[ERROR] lang.initialize(): Reading YAML file <#{filename}>"
+      Tool.instance.verbose "[ADVISE] Perhaps you use apostrophe into string without \\ character"
       raise e
     end
     filename=File.join( dirbase, "locales", @lang, "connectors.yaml" )
