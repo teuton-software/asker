cc379cabe1a9d10d37f9e59b4c637ddd9566b4d7
David Vargas <davidvargas.tenerife@gmail.com>
2016-07-27 09:05:10 +0100

    U

--- lib/tool.rb
+++ lib/tool.rb
@@ -3,13 +3,11 @@
 
 require 'singleton'
 require 'yaml'
-require 'haml'
 require 'rainbow'
-require 'rexml/document'
 
+require_relative 'project'
 require_relative 'concept/concept'
 require_relative 'tool/create_actions'
-require_relative 'tool/log_actions'
 require_relative 'tool/input_actions'
 require_relative 'tool/show_actions'
 
@@ -17,67 +15,57 @@ class Tool
   include Singleton
   include CreateActions
   include InputActions
-  include LogActions
   include ShowActions
 
   def run(pArgs={})
     init pArgs
-    create_log_file
     load_input_files
-    show_data if Application.instance.show_mode!=:none
+    show_data if Project.instance.show_mode!=:none
 	  create_output_files
 	  show_stats
 	  close_log_file
   end
 
   def init(pArgs={})
-	  app=Application.instance
+	  project=Project.instance
 
     if pArgs.class==Hash then
-      app.param.merge!(pArgs)
+      project.param.merge!(pArgs)
     elsif pArgs.class==String then
       if not File.exist?(pArgs)
-        verbose Rainbow("[WARN] Tool.init: ").yellow+Rainbow(pArgs).yellow.bright+Rainbow(" dosn't exists!").yellow
+        project.verbose Rainbow("[WARN] Tool.init: ").yellow+Rainbow(pArgs).yellow.bright+Rainbow(" dosn't exists!").yellow
         exit 1
       end
 
       if pArgs.include?(".haml") then
-        app.param[:inputdirs]      = File.dirname(pArgs)
-        app.param[:process_file]   = File.basename(pArgs)
+        project.param[:inputdirs]      = File.dirname(pArgs)
+        project.param[:process_file]   = File.basename(pArgs)
       elsif pArgs.include?(".yaml") then
-        app.param=YAML::load(File.open(pArgs))
-        app.param[:configfilename]=pArgs
+        project.param=YAML::load(File.open(pArgs))
+        project.param[:configfilename]=pArgs
         a=pArgs.split(File::SEPARATOR)
         a.delete_at(-1)
-        app.param[:projectdir]=a.join("/")
+        project.param[:projectdir]=a.join(File::SEPARATOR)
       elsif File.directory?(pArgs) then
-        verbose Rainbow("[WARN] Tool.init: Directory input ").yellow+Rainbow(pArgs).bright.yellow+Rainbow(" not implemented yet").yellow
+        project.verbose Rainbow("[WARN] Tool.init: Directory input ").yellow+Rainbow(pArgs).bright.yellow+Rainbow(" not implemented yet").yellow
         exit 1
         #app.param[:inputdirs]=pArgs
         #app.param[:process_file]=Dir.entries(pArgs)
       else
-        verbose Rainbow("[ERR ] Tool.init: Input ").red+Rainbow(pArgs).red.bright+Rainbow(" unkown").red
+        project.verbose Rainbow("[ERR ] Tool.init: Input ").red+Rainbow(pArgs).red.bright+Rainbow(" unkown").red
         exit 1
       end
     else
-      verbose Rainbow("[ERROR] Tool.init: Configuration params format is <#{pArgs.class.to_s}>!").red
+      project.verbose Rainbow("[ERROR] Tool.init: Configuration params format is <#{pArgs.class.to_s}>!").red
       exit
     end
 
-    app.fill_param_with_values
-    verbose Rainbow("Initial Params:").blue.bright
-    verbose Rainbow("  * inputdirs    = #{app.param[:inputdirs]}").blue.bright
-    verbose Rainbow("  * process_file = #{app.param[:process_file]}").blue.bright
+    project.fill_param_with_values
+    project.verbose Rainbow("Initial Params:").blue.bright
+    project.verbose Rainbow("  * inputdirs    = #{project.param[:inputdirs]}").blue.bright
+    project.verbose Rainbow("  * process_file = #{project.param[:process_file]}").blue.bright
 
     @concepts={}
-
-    @logname=app.outputdir+'/'+app.logname
-    @outputname=app.outputdir+'/'+app.outputname
-  end
-
-  def verbose(lsText)
-    puts lsText
-    @logfile.write(lsText.to_s+"\n") if @logfile
   end
 
 end
