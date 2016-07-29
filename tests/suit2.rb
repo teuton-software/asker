65e48c5da850cc55b993322af24ffb359be12d85
David Vargas <davidvargas.tenerife@gmail.com>
2016-07-29 16:00:23 +0100

    D files version2 temporal

--- tests/suit2.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/ruby
-
-require 'minitest/autorun'
-
-#require_relative 'application_test'
-#require_relative 'lang_test'
-require_relative 'concept2_test'
-require_relative 'question2_test'
