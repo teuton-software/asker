1ce6d1f26d7663ca358c20b3dfd72e342d6916f2
David Vargas <davidvargas.tenerife@gmail.com>
2016-07-30 11:51:37 +0100

    U move method from caoncept class to ia module

--- lib/concept/concept.rb
+++ lib/concept/concept.rb
@@ -74,64 +74,15 @@ class Concept
   end
 
   def try_adding_neighbor(pConcept)
+
+    #TODO:!? this method comes from module ia_calculate
     p = calculate_nearness_to_concept(pConcept)
+
     return if p==0
     @data[:neighbors]<< { :concept => pConcept , :value => p }
     #Sort neighbors list
     @data[:neighbors].sort! { |a,b| a[:value] <=> b[:value] }
     @data[:neighbors].reverse!
-  end
-
-  def make_questions_from_ia
-    return if @process==false
-
-    #Stage A: IA process every <def> definition
-    @questions[:stage_a] = run_stage_a
-    @questions[:stage_b] = []
-    @questions[:stage_c] = []
-    @questions[:stage_d] = []
-    @questions[:stage_e] = []
-
-    #IA process every table of this concept
-    tables.each do |lTable|
-      #create <list1> with all the rows from the table
-      list1=[]
-      count=1
-      lTable.rows.each do |i|
-        list1 << { :id => count, :name => @name, :weight => 0, :data => i }
-        count+=1
-      end
-
-      #create a <list2> with similar rows (same table name) from the neighbours
-      list2=[]
-      neighbors.each do |n|
-        n[:concept].tables.each do |t2|
-          if t2.name==lTable.name then
-            t2.rows.each do |i|
-              list2 << { :id => count, :name => n[:concept].name, :weight => 0, :data => i }
-              count+=1
-            end
-          end
-        end
-      end
-
-      list3=list1+list2
-
-      #Stage B: process table match
-      @questions[:stage_b] = @questions[:stage_b] + run_stage_b(lTable, list1, list2)
-
-      #Stage C: process_tableXfields
-      list1.each do |lRow|
-        reorder_list_with_row(list3, lRow)
-        @questions[:stage_c] = @questions[:stage_c] + run_stage_c(lTable, lRow, list3)
-      end
-
-      #Stage D: process table1field
-      @questions[:stage_d] = @questions[:stage_d] + run_stage_d(lTable, list1, list2)
-
-      #Stage E: process sequence
-      @questions[:stage_e] = @questions[:stage_e] + run_stage_e(lTable, list1, list2)
-    end
   end
 
   def method_missing(m, *args, &block)
