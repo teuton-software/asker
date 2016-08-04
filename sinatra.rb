2883c4ef73b76794c23a930d1f3e1da79f4fab9b
David Vargas <davidvargas.tenerife@gmail.com>
2016-08-04 22:55:33 +0100

    N sinatra web gui interface. For now we search files

---
+++ sinatra.rb
@@ -0,0 +1,59 @@
+#!/usr/bin/ruby
+
+require "sinatra/base"
+
+class MyApp < Sinatra::Base
+  BASEDIR="./input"
+
+  get '/' do
+    redirect '/ls'
+  end
+
+  get '/ls' do
+    @current=File.join(BASEDIR)
+    load_dir
+    erb :list
+  end
+
+  get '/ls/*' do
+    @current=File.join(BASEDIR, params[:splat])
+    load_dir
+    erb :list
+  end
+
+  def load_dir
+    @filenames = Dir[@current+"/**"].sort!
+  end
+
+  helpers do
+    def route_for(path)
+      items=path.split(File::SEPARATOR)
+      items.delete(".")
+      items.delete("input")
+      return items.join(File::SEPARATOR)
+    end
+
+    def html_for_current
+      items=@current.split(File::SEPARATOR)
+      items.delete(".")
+      items.delete("input")
+
+      output=""
+      before=""
+      items.each do |i|
+        if i==items.last then
+          output += " | "+i
+        else
+          before=before+"/"+i
+          output += " | <a href=\"/ls"+before+"/\">"+i+"</a>"
+        end
+      end
+      return output
+    end
+  end
+
+  run!
+end
+
+#app = MyApp.new
+#app.run!
