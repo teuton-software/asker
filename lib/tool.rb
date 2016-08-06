5c63c1d2b743c96505cb35c53fd7140493477f74
David Vargas <davidvargas.tenerife@gmail.com>
2016-08-06 20:34:02 +0100

    U

--- lib/tool.rb
+++ lib/tool.rb
@@ -23,33 +23,44 @@ class Tool
   end
 
   def start(pArgs={})
+    load_input_data(pArgs)
+    create_output_files
+    ConceptIAScreenFormatter.new(@concepts_ia).export
+	  Project.instance.close
+  end
+
+  def load_input_data(pArgs)
     ProjectLoader::load(pArgs)
     Project.instance.open
     @concepts = InputLoader.new.load
     ConceptScreenFormatter.new(@concepts).export
-
-    create_output_files
-    ConceptIAScreenFormatter.new(@concepts_ia).export
-
-	  Project.instance.close
   end
 
- def create_output_files
-   Project.instance.verbose "\n"
-   Project.instance.verbose "[INFO] Creating output files..."
-   Project.instance.verbose "   ├── Gift questions file = "+Rainbow(Project.instance.outputpath).bright
-   Project.instance.verbose "   └── Lesson file         = "+Rainbow(Project.instance.lessonpath).bright
+  def create_output_files
+    Project.instance.verbose "\n"
+    Project.instance.verbose "[INFO] Creating output files..."
+    Project.instance.verbose "   ├── Gift questions file = "+Rainbow(Project.instance.outputpath).bright
+    Project.instance.verbose "   └── Lesson file         = "+Rainbow(Project.instance.lessonpath).bright
 
-   @concepts.each do |concept|
-     concept_ia = ConceptIA.new(concept)
-     concept_ia.make_questions_from_ia
-     ConceptGiftFormatter.new(concept_ia).export
-     @concepts_ia << concept_ia
-   end
+    create_questions
+    create_lesson
+  end
 
-   @concepts_ia.each do |concept_ia|
-     ConceptDocFormatter.new(concept_ia.concept).export
-   end
- end
+private
+
+  def create_questions
+    @concepts.each do |concept|
+      concept_ia = ConceptIA.new(concept)
+      concept_ia.make_questions_from_ia
+      ConceptGiftFormatter.new(concept_ia).export
+      @concepts_ia << concept_ia
+    end
+  end
+
+  def create_lesson
+    @concepts_ia.each do |concept_ia|
+      ConceptDocFormatter.new(concept_ia.concept).export
+    end
+  end
 
 end
