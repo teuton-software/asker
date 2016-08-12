d85c583729dd2c7a3e380760ea4c188eb6e9bf25
David Vargas <davidvargas.tenerife@gmail.com>
2016-08-12 13:32:16 +0100

    N bootstrap into sinatra app

--- lib/sinatra/helpers.rb
+++ lib/sinatra/helpers.rb
@@ -3,41 +3,41 @@ module Sinatra
   module SinatraFrontEnd
     module Helpers
 
-    BASEDIR=Project.instance.inputbasedir
+      BASEDIR=Project.instance.inputbasedir
 
-    def route_for(path)
-      items=path.split(File::SEPARATOR)
-      items.delete(".")
-      items.delete(BASEDIR)
-      return items.join(File::SEPARATOR)
-    end
-
-    def remove_basedir(dir)
-      items=@current.split(File::SEPARATOR)
-      items.delete(".")
-      items.delete("..")
-      items.delete(BASEDIR)
-      return File.join(items,File::SEPARATOR)
-    end
-
-    def html_for_current( option={ :indexlast => false} )
-      items=@current.split(File::SEPARATOR)
-      items.delete(".")
-      items.delete("..")
-      items.delete(BASEDIR)
-
-      output=""
-      before=""
-      items.each do |i|
-        if i==items.last and option[:indexlast]==false then
-          output += " » "+i
-        else
-          before=before+"/"+i
-          output += " » <a href=\"/dir/list"+before+"/\">"+i+"</a>"
-        end
+      def route_for(path)
+        items=path.split(File::SEPARATOR)
+        items.delete(".")
+        items.delete(BASEDIR)
+        return items.join(File::SEPARATOR)
       end
-      return output
-    end
+
+      def remove_basedir(dir)
+        items=@current.split(File::SEPARATOR)
+        items.delete(".")
+        items.delete("..")
+        items.delete(BASEDIR)
+        return File.join(items,File::SEPARATOR)
+      end
+
+      def html_for_current( option={ :indexlast => false} )
+        items=@current.split(File::SEPARATOR)
+        items.delete(".")
+        items.delete("..")
+        items.delete(BASEDIR)
+
+        output="<a href=\"/dir/list\">Home</a>"
+        before=""
+        items.each do |i|
+          if i==items.last and option[:indexlast]==false then
+            output += "/"+i
+          else
+            before=before+"/"+i
+            output += "/<a href=\"/dir/list"+before+"/\">"+i+"</a>"
+          end
+        end
+        return output
+      end
 
     end
   end
