228ecc595fa0907d94a5f088c4fa038483adb34f
David Vargas <davidvargas.tenerife@gmail.com>
2016-08-14 22:28:51 +0100

    U

--- lib/lang/lang.rb
+++ lib/lang/lang.rb
@@ -7,13 +7,13 @@ require_relative 'text_actions'
 class Lang
   include TextActions
 
-  attr_accessor :lang
+  attr_accessor :locale
 
-  def initialize(pLang='en')
-    @lang=pLang.to_s
+  def initialize(locale='en')
+    @locale = locale.to_s
 
     dirbase = File.dirname(__FILE__)
-    filename=File.join( dirbase, "locales", @lang ,"templates.yaml" )
+    filename=File.join( dirbase, "locales", @locale ,"templates.yaml" )
     begin
       @templates=YAML::load(File.new(filename))
     rescue Exception => e
@@ -21,11 +21,15 @@ class Lang
       Project.instance.verbose "[ADVISE] Perhaps you use apostrophe into string without \\ character\n"
       raise e
     end
-    filename=File.join( dirbase, "locales", @lang, "connectors.yaml" )
+    filename=File.join( dirbase, "locales", @locale, "connectors.yaml" )
     @connectors=YAML::load(File.new(filename))
 
-    filename=File.join( dirbase, "locales", @lang, "mistakes.yaml" )
+    filename=File.join( dirbase, "locales", @locale, "mistakes.yaml" )
     @mistakes=YAML::load(File.new(filename))
+  end
+
+  def lang
+    @locale
   end
 
 end
