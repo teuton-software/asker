8d502f8763dd2339fb77da9efacca74497e86fea
David Vargas <davidvargas.tenerife@gmail.com>
2016-08-20 00:49:39 +0100

    U

--- lib/ai/stages/stage_s.rb
+++ lib/ai/stages/stage_s.rb
@@ -8,10 +8,7 @@ class StageS < BaseStage
   def run(pTable, pList1, pList2)
     #process_sequence
     questions = []
-
-    if not ( pTable.fields.count==1 and pTable.sequence?)
-      return questions
-    end
+    return questions unless ( pTable.fields.count==1 and pTable.sequence? and pTable.sequence[0]!="")
 
     #TODO
     #items=[]
@@ -19,12 +16,12 @@ class StageS < BaseStage
     #puts Rainbow(items).blue.bright
 
     #Question type <d3sequence>: items are part of a sequence
-    if pList1.count>3 and pTable.sequence[0]!=""
+    if pList1.count>3
 	    a=0..(pList1.count-4)
 	    a.each_entry do |i|
         q=Question.new(:match)
         q.name="#{name}-#{num.to_s}-s1sequence-#{pTable.name}"
-        q.text=lang.text_for(:s1, name, pTable.fields[0].capitalize, pTable.sequence[0] )
+        q.text= random_image_for(name) + lang.text_for(:s1, name, pTable.fields[0].capitalize, pTable.sequence[0] )
         q.matching << [ pList1[i+0][:data][0], '1º' ]
         q.matching << [ pList1[i+1][:data][0], '2º' ]
         q.matching << [ pList1[i+2][:data][0], '3º' ]
@@ -40,7 +37,7 @@ class StageS < BaseStage
         q=Question.new
         q.set_match
         q.name="#{name}-#{num.to_s}-s2sequence-#{pTable.name}"
-        q.text=lang.text_for(:s1, name, pTable.fields[0].capitalize, pTable.sequence[1] )
+        q.text= random_image_for(name) + lang.text_for(:s1, name, pTable.fields[0].capitalize, pTable.sequence[1] )
         q.matching << [ pList1[i+3][:data][0], '1º' ]
         q.matching << [ pList1[i+2][:data][0], '2º' ]
         q.matching << [ pList1[i+1][:data][0], '3º' ]
