e0fedcf58c2de764742204a029d9619751ab2ae4
David Vargas <davidvargas.tenerife@gmail.com>
2016-08-21 20:50:12 +0100

    U add row test

--- tests/suit.rb
+++ tests/suit.rb
@@ -10,7 +10,9 @@ require_relative 'lang/lang_factory_test'
 
 require_relative 'data/concept_test'
 require_relative 'data/table_test'
+require_relative 'data/row_test'
 require_relative 'data/world_test'
+
 require_relative 'ai/question_test'
 require_relative 'ai/concept_ai_test'
 
