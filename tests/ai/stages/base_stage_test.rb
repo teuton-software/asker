9287c1ea2a8dfbb0f6b8f39afac08c35af46637f
David Vargas <davidvargas.tenerife@gmail.com>
2016-08-27 16:21:20 +0100

    N test

---
+++ tests/ai/stages/base_stage_test.rb
@@ -0,0 +1,113 @@
+#!/usr/bin/ruby
+
+require "minitest/autorun"
+require 'rexml/document'
+
+require_relative "../../../lib/ai/stages/base_stage"
+require_relative "../../../lib/data/concept"
+require_relative "../../../lib/data/world"
+require_relative "../../../lib/ai/concept_ai"
+require 'pry'
+
+class BaseStageTest < Minitest::Test
+  def setup
+    string_data = get_xml_data
+    concepts    = []
+    world       = World.new(concepts, false)
+    root_xml_data=REXML::Document.new(string_data)
+    root_xml_data.root.elements.each do |xml_data|
+      if xml_data.name="concept" then
+        concepts << Concept.new(xml_data, "input.haml", "en", [])
+      end
+    end
+
+    @concepts_ai=[]
+    concepts.each { |concept| @concepts_ai << ConceptAI.new(concept,world) }
+    @base_stages=[]
+    @concepts_ai.each { |concept_ai| @base_stages << BaseStage.new(concept_ai) }
+  end
+
+  def test_base_stage_0_delegating
+      b = @base_stages[0]
+      c = @concepts_ai[0]
+      assert_equal "text"      , b.type
+      assert_equal c.type      , b.type
+      assert_equal "en"        , b.lang.code
+      assert_equal c.lang.code , b.lang.code
+  end
+
+  def get_xml_data
+    string_data=<<EOF
+    <map lang='en' context='character, starwars' version='1'>
+
+      <concept>
+        <names>obiwan</names>
+        <tags>jedi, teacher, annakin, skywalker, pupil, quigon-jinn</tags>
+        <def>Jedi, teacher of Annakin  Skywalker</def>
+        <def>Jedi, pupil of Quigon-Jinn</def>
+        <table fields='attribute, value'>
+          <row>
+            <col>race</col>
+            <col>human</col>
+          </row>
+          <row>
+            <col>laser sabel color</col>
+            <col>green</col>
+          </row>
+          <row>
+            <col>hair color</col>
+            <col>red</col>
+          </row>
+        </table>
+      </concept>
+
+      <concept>
+        <names>yoda</names>
+        <tags>teacher, jedi</tags>
+        <def>Jedi, teacher of all jedis</def>
+        <def>The Main Teacher of Jedi and one of the most important members of the Main Jedi Council, in the last days of Star Republic.</def>
+        <def>He has exceptional combat abilities with light sable, using acrobatics tecnics from Ataru.</def>
+        <def>He was master of all light sable combat styles and was considered during years as a Sword Master.</def>
+        <table fields='attribute, value'>
+          <row>
+            <col>laser sabel color</col>
+            <col>green</col>
+          </row>
+          <row>
+            <col>hair color</col>
+            <col>white</col>
+          </row>
+          <row>
+            <col>skin color</col>
+            <col>green</col>
+          </row>
+          <row>
+            <col>high</col>
+            <col>65 centimetres</col>
+          </row>
+        </table>
+      </concept>
+
+      <concept>
+        <names>maul</names>
+        <tags>lord, sith</tags>
+        <def type="image_url">http://maul.pgn</def>
+      </concept>
+
+      <concept>
+        <names>starwars films</names>
+        <tags>starwars, films</tags>
+        <table fields='film name' sequence='Films ordered by episode number'>
+          <row>The Phantom Menace</row>
+          <row>Attack of the Clones</row>
+          <row>Revenge of the Sith</row>
+          <row>A New Hope</row>
+          <row>The Empire Strikes Back</row>
+          <row>Return of the Jedi</row>
+          <row>The Force Awakens</row>
+        </table>
+      </concept>
+    </map>
+EOF
+  end
+end
