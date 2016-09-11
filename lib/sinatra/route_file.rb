76b7b827c320c4926df498f3abfaa1965fc016c6
David Vargas <davidvargas.tenerife@gmail.com>
2016-09-11 22:22:52 +0100

    U delete file action

--- lib/sinatra/route_file.rb
+++ lib/sinatra/route_file.rb
@@ -8,13 +8,20 @@ module Sinatra
       def self.registered(app)
         app.get '/file/show/*.*' do |path,ext|
           @filename = path+"."+ext
-          filepath = File.join( Project.instance.inputbasedir , @filename)
+          filepath = File.join( Project.instance.inputbasedir, @filename)
           content = load_file filepath
 #          @filecontent = CodeRay.scan(content, ext.to_sym).div(:line_numbers => :table)
           @filecontent = "<pre>#{content}</pre>"
           @concepts = session['concepts']
           @current = File.dirname(filepath)
           erb :"file/show"
+        end
+
+        app.get '/file/delete/*' do
+          filepath = File.join( Project.instance.inputbasedir, params[:splat] )
+          dirpath = File.dirname(filepath)
+          FileUtils.rm(filepath)
+          redirect "/dir/list/#{route_for(dirpath)}"
         end
 
         app.post '/file/new' do
