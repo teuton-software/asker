3c4db10bf8b9ecad2073e9178aebffdae49d3766
David Vargas <davidvargas.tenerife@gmail.com>
2016-11-21 19:10:00 +0000

    U test attr_reader

--- tests/data/data_field_test.rb
+++ tests/data/data_field_test.rb
@@ -11,6 +11,8 @@ class DataFieldTest < Minitest::Test
   def test_data_0
     d = DataField.new('obiwan', 0, 'text')
 
+    assert_equal 0,        d.id
+    assert_equal :text,    d.type
     assert_equal 'obiwan', d.get(:raw)
     assert_equal 'obiwan', d.get(:decorated)
     assert_equal 'obiwan', d.get(:id)
@@ -21,6 +23,8 @@ class DataFieldTest < Minitest::Test
     url = 'http://www1/url/to/textfile.rb'
     d = DataField.new(url, 1, 'textfile_url')
 
+    assert_equal 1,                                     d.id
+    assert_equal :textfile_url,                         d.type
     assert_equal url,                                   d.get(:raw)
     assert_equal "<a href=\"#{url}\">Textfile URL</a>", d.get(:decorated)
     assert_equal 'textfile_url.1',                      d.get(:id)
@@ -31,6 +35,8 @@ class DataFieldTest < Minitest::Test
     url = 'http://www2/url/to/image.png'
     d = DataField.new(url, 2, 'image_url')
 
+    assert_equal 2,                                    d.id
+    assert_equal :image_url,                           d.type
     assert_equal url,                                  d.get(:raw)
     assert_equal "<img src=\"#{url}\" alt=\"image\">", d.get(:decorated)
     assert_equal 'image_url.2',                        d.get(:id)
@@ -41,9 +47,11 @@ class DataFieldTest < Minitest::Test
     longname = 'this-is-just-a-very-0123456789-very-long-name'
     d = DataField.new(longname, 0, 'text')
 
-    assert_equal longname, d.get(:raw)
-    assert_equal longname, d.get(:decorated)
-    assert_equal longname, d.get(:id)
+    assert_equal 0,                           d.id
+    assert_equal :text,                       d.type
+    assert_equal longname,                    d.get(:raw)
+    assert_equal longname,                    d.get(:decorated)
+    assert_equal longname,                    d.get(:id)
     assert_equal 'this-is...-very-long-name', d.get(:screen)
   end
 end
