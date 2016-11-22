734d4b70acce3516f2979756999c110ae4816315
David Vargas <davidvargas.tenerife@gmail.com>
2016-11-22 06:38:45 +0000

    U reduce rubocop ofenses

--- lib/loader/image_url_loader.rb
+++ lib/loader/image_url_loader.rb
@@ -3,51 +3,57 @@ require 'net/http'
 require 'uri'
 require_relative '../project'
 
+# Search URL images on Internet
+# Methods:
+# * +load+ - Accept String or an Array with the desired search
+# * +sanitize_string+ - Clean URL string
+# * +sanitize_array+ - Clean URL Array
 module ImageUrlLoader
-
-  def self.load(input=[])
+  def self.load(input = [])
     filters = []
-    if input.class==String then
-      filters += sanitize_string( input.clone )
+    if input.class == String
+      filters += sanitize_string(input.clone)
       filters.flatten!
-    elsif input.class==Array then
-      filters = sanitize_array( input.clone )
+    elsif input.class == Array
+      filters = sanitize_array(input.clone)
       filters.flatten!
     else
-      raise "[ERROR] ImageUrlLoader: Unkown type #{input.class.to_s}"
+      raise "[ERROR] ImageUrlLoader: Unkown type #{input.class}"
     end
-    #Search Image URLs from Google site, selected by <filters>
-    search_url="https://www.google.es/search?q=#{filters.join("+").to_s}&source=lnms&tbm=isch&sa=X&ved=0ahUKEwie9ruF5KLOAhXCOBQKHY-QBTcQ_AUICCgB&biw=1366&bih=643"
+    # Search Image URLs from Google site, selected by <filters>
+    search_url = 'https://www.google.es/search?q='
+    search_url << filters.join('+').to_s
+    search_url << '&source=lnms&tbm=isch&sa=X&ved=0ahUKEwie9ruF5KLOAhXCOBQKHY-QBTcQ_AUICCgB&biw=1366&bih=643'
+
     image_urls = []
     begin
       uri = URI.parse(search_url)
       response = Net::HTTP.get_response(uri)
 
-      r = response.body.split(" ")
+      r = response.body.split(' ')
       r.each do |line|
-        if line.include? "src=\"https"
-          image_urls << line.gsub("\"","")[ 4, line.size]
+        if line.include? 'src="https'
+          image_urls << line.delete('"')[4, line.size]
         end
       end
     rescue
-      Project.instance.verbose "[ERROR] ImageUrlLoader"
+      Project.instance.verbose '[ERROR] ImageUrlLoader'
       Project.instance.verbose " => #{search_url}"
-      Project.instance.verbose " => Check Internet connections"
-      Project.instance.verbose " => Ensure URL is well formed"
+      Project.instance.verbose ' => Check Internet connections'
+      Project.instance.verbose ' => Ensure URL is well formed'
     end
-  return image_urls
+    image_urls
   end
 
   def self.sanitize_string(input)
-    r = [ ["á","a"], ["é","e"], ["í","i"], ["ó","o"], ["ú","u"], ["ñ","n"], ["Á","A"], ["É","E"], ["Í","I"], ["Ó","O"], ["Ú","U"]]
+    r = [ ['á', 'a'], ['é', 'e'], ['í', 'i'], ['ó', 'o'], ['ú', 'u'], ['ñ', 'n'], ['Á', 'A'], ['É', 'E'], ['Í', 'I'], ['Ó', 'O'], ['Ú', 'U'], ['Ñ', 'N']]
     r.each { |item| input.gsub!(item[0], item[1]) }
-    r = [ "-", "_", ",", "\"" ]
-    r.each { |item| input.gsub!(item, " ")}
-    return input.split(" ")
+    r = ['-', '_', ',', '"']
+    r.each { |item| input.gsub!(item, ' ') }
+    input.split(' ')
   end
 
   def self.sanitize_array(input)
-    output = input.map { |i| sanitize_string(i) }
+    input.map { |i| sanitize_string(i) }
   end
-
 end
