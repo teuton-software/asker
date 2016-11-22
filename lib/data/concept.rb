e33bca21c6404fc8f004662bae2a033c43619746
David Vargas <davidvargas.tenerife@gmail.com>
2016-11-22 20:25:13 +0000

    U reduce rubocop offenses

--- lib/data/concept.rb
+++ lib/data/concept.rb
@@ -15,37 +15,38 @@ class Concept
   attr_reader :data
   attr_accessor :process
 
-  @@id=0
+  @@id = 0
 
-  def initialize(pXMLdata, pFilename, langCode="en", pContext=[])
-    @@id+=1
-    @id=@@id
+  def initialize(xml_data, filename, lang_code = 'en', context = [])
+    @@id += 1
+    @id = @@id
 
-    @filename = pFilename
+    @filename = filename
     @process  = false
-    @lang     = LangFactory.instance.get(langCode)
+    @lang     = LangFactory.instance.get(lang_code)
 
-    if pContext.class==Array then
-      @context=pContext
-    elsif pContext.nil? then
-      @context=[]
+    if context.class == Array
+      @context = context
+    elsif context.nil?
+      @context = []
     else
-      @context=pContext.split(",")
-      @context.collect! { |i| i.strip }
+      @context = context.split(',')
+      @context.collect!(&:strip)
     end
-    @names = [ 'concept.'+@id.to_s ]
-    @type  = "text"
+    @names = ['concept.' + @id.to_s ]
+    @type  = 'text'
 
-    @data={}
-    @data[:tags]=[]
-    @data[:texts]=[]
-    @data[:images]=[] #TODO: By now, We'll treat images separated from texts
-    @data[:tables]=[]
-    @data[:neighbors]=[]
-    @data[:reference_to]=[]
-    @data[:referenced_by]=[]
+    @data = {}
+    @data[:tags] = []
+    @data[:texts] = []
+    @data[:images] = []         # TODO: By now, We'll treat images separated from texts
+    @data[:textfile_paths] = [] # TODO: By now, We'll treat this separated from texts
+    @data[:tables] = []
+    @data[:neighbors] = []
+    @data[:reference_to] = []
+    @data[:referenced_by] = []
 
-	  read_data_from_xml(pXMLdata)
+	  read_data_from_xml(xml_data)
   end
 
   def name(option=:raw)
@@ -62,91 +63,120 @@ class Concept
 
   def try_adding_neighbor(other)
     p = calculate_nearness_to_concept(other)
-    return if p==0
+    return if p.zero?
     @data[:neighbors] << { :concept => other , :value => p }
-    #Sort neighbors list
+    # Sort neighbors list
     @data[:neighbors].sort! { |a,b| a[:value] <=> b[:value] }
     @data[:neighbors].reverse!
   end
 
   def calculate_nearness_to_concept(other)
-    weights=Project.instance.formula_weights
+    weights = Project.instance.formula_weights
 
-    liMax1=@context.count
-    liMax2=@data[:tags].count
-    liMax3=@data[:tables].count
+    li_max1 = @context.count
+    li_max2 = @data[:tags].count
+    li_max3 = @data[:tables].count
 
-    lfAlike1=lfAlike2=lfAlike3=0.0
+    lf_alike1 = lf_alike2 = lf_alike3 = 0.0
 
     # check if exists this items from concept1 into concept2
-    @context.each { |i| lfAlike1+=1.0 if !other.context.index(i).nil? }
-    @data[:tags].each { |i| lfAlike2+=1.0 if !other.tags.index(i).nil? }
-    @data[:tables].each { |i| lfAlike3+=1.0 if !other.tables.index(i).nil? }
+    @context.each { |i| lf_alike1 += 1.0 unless other.context.index(i).nil? }
+    @data[:tags].each { |i| lf_alike2 += 1.0 unless other.tags.index(i).nil? }
+    @data[:tables].each { |i| lf_alike3 += 1.0 unless other.tables.index(i).nil? }
 
-    lfAlike = ( lfAlike1*weights[0] + lfAlike2*weights[1] + lfAlike3*weights[2] )
-    liMax   = ( liMax1  *weights[0] + liMax2  *weights[1] + liMax3*weights[2]   )
-    return ( lfAlike*100.0/ liMax )
+    lf_alike = (lf_alike1 * weights[0] + lf_alike2 * weights[1] + lf_alike3 * weights[2])
+    li_max = (li_max1 * weights[0] + li_max2 * weights[1] + li_max3 * weights[2])
+    (lf_alike * 100.0 / li_max)
   end
 
   def try_adding_references(other)
-    reference_to=0
-    @data[:tags].each { |i| reference_to+=1 if !other.names.index(i.downcase).nil? }
+    reference_to = 0
+    @data[:tags].each { |i| reference_to += 1 unless other.names.index(i.downcase).nil? }
     @data[:texts].each do |t|
-      text=t.clone
-      text.split(" ").each do |word|
-        reference_to+=1 if !other.names.index(word.downcase).nil?
+      text = t.clone
+      text.split(' ').each do |word|
+        reference_to += 1 unless other.names.index(word.downcase).nil?
       end
     end
-    if reference_to>0 then
+    if reference_to > 0
       @data[:reference_to] << other.name
-      other.data[:referenced_by] << self.name
+      other.data[:referenced_by] << name
     end
   end
 
-  def method_missing(m, *args, &block)
-    return @data[m]
+  def method_missing(m)
+    @data[m]
   end
 
-private
+  private
 
-  def read_data_from_xml(pXMLdata)
-    pXMLdata.elements.each do |i|
+  def read_data_from_xml(xml_data)
+    xml_data.elements.each do |i|
       case i.name
       when 'names'
-        @names = []
-        j=i.text.split(",")
-        j.each { |k| @names << k.strip }
-        @type = i.attributes['type'].strip if i.attributes['type']
+        process_names(i)
       when 'tags'
-        raise '[Error] tags label empty!' if i.text.nil? or i.text.size==0
-        @data[:tags]=i.text.split(",")
-        @data[:tags].collect! { |k| k.strip }
+        process_tags(i)
       when 'context'
-        #DEPRECATED: Don't use xml tag <context> instead define it as attibute of root xml tag
-        msg="   │  "+Rainbow(" [DEPRECATED] Concept ").yellow+Rainbow(name).yellow.bright+Rainbow(" use XMLtag <context>. Instead define it as root attibute.").yellow
-        Project.instance.verbose msg
-        @context=i.text.split(",")
-        @context.collect! { |k| k.strip }
+        # DEPRECATED: Don't use xml tag <context>
+        # instead define it as attibute of root xml tag
+        process_context(i)
       when 'text'
-        #DEPRECATED: Use xml tag <def> instead of <text>
-        msg="   │  "+Rainbow(" [DEPRECATED] Concept ").yellow+Rainbow(name).yellow.bright+Rainbow(" use XMLtag <text>, Instead use <def> tag.").yellow
-        Project.instance.verbose msg
-        @data[:texts] << i.text.strip
+        # DEPRECATED: Use xml tag <def> instead of <text>
+        process_text(i)
       when 'def'
-        case i.attributes['type']
-        when 'image'
-          Project.instance.verbose Rainbow("[DEBUG] Concept#read_xml: image #{Rainbow(i.text).bright}").yellow
-        when 'image_url'
-          @data[:images] << i.text.strip
-        else
-          @data[:texts] << i.text.strip
-        end
+        process_def(i)
       when 'table'
         @data[:tables] << Table.new(self, i)
       else
-        msg = Rainbow("   [ERROR] <#{i.name}> attribute into XMLdata (concept#read_data_from_xml)").color(:red)
+        text = "   [ERROR] <#{i.name}> unkown XML attribute for concept #{name}"
+        msg = Rainbow(text).color(:red)
         Project.instance.verbose msg
       end
+    end
+  end
+
+  def process_names(i)
+    @names = []
+    j = i.text.split(',')
+    j.each { |k| @names << k.strip }
+    @type = i.attributes['type'].strip if i.attributes['type']
+  end
+
+  def process_tags(i)
+    raise '[Error] tags label empty!' if i.text.nil? || i.text.size.zero?
+    @data[:tags] = i.text.split(',')
+    @data[:tags].collect!(&:strip)
+  end
+
+  def process_context(i)
+    msg = '   │  ' + Rainbow(' [DEPRECATED] Concept ').yellow
+    msg += Rainbow(name).yellow.bright
+    msg += Rainbow(' move <context> tag info to <map>.').yellow
+    Project.instance.verbose msg
+    @context = i.text.split(',')
+    @context.collect!(&:strip)
+  end
+
+  def process_text(i)
+    msg = '   │  ' + Rainbow(' [DEPRECATED] Concept ').yellow
+    msg += Rainbow(name).yellow.bright
+    msg += Rainbow(' replace <text> tag by <def>.').yellow
+    Project.instance.verbose msg
+    @data[:texts] << i.text.strip
+  end
+
+  def process_def(i)
+    case i.attributes['type']
+    when 'image'
+      msg = "[DEBUG] Concept#read_xml: image #{Rainbow(i.text).bright}"
+      Project.instance.verbose Rainbow(msg).yellow
+    when 'image_url'
+      @data[:images] << i.text.strip
+    when 'textfile_path'
+      @data[:textfile_paths] << i.text.strip
+    else
+      @data[:texts] << i.text.strip
     end
   end
 end
