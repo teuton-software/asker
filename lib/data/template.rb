bfe4177d336046a464dbc2c9da042cdde5560a7d
David Vargas <davidvargas.tenerife@gmail.com>
2016-11-29 18:52:38 +0000

    W working with template class

--- lib/data/template.rb
+++ lib/data/template.rb
@@ -9,35 +9,39 @@ class Template
     @table   = table
     @index   = index
     @rows    = []
-    @xml     = xml
-    load_info_from(xml)
-    @output = use_vars_into_template
-    debug
-    @rows
+    vars, template = load_info_from(xml)
+    data_string = apply_vars_to_template(vars, template)
+    @rows = read_rows_from(data_string)
   end
 
   def load_info_from(xml)
-    @vars = {}
-    vars = xml.attributes
-    vars.keys.each { |i| @vars[i]=vars[i].split(',') }
-    @template = ''
-    xml.elements.each { |i| @template << i.to_s + "\n" }
+    vars = {}
+    template = ''
+    v = xml.attributes
+    v.keys.each { |i| vars[i]=v[i].split(',') }
+    xml.elements.each { |i| template << i.to_s + "\n" }
+    return vars, template
   end
 
-  def use_vars_into_template
+  def apply_vars_to_template(vars,template)
     output = ''
-    return if @vars.size.zero?
-    max = @vars.first[1].size
+    return if vars.size.zero?
+    max = vars.first[1].size
     (1..max).each do |index|
-      @vars.each_pair { |k,v| output += @template.dup.gsub!(k,v[index-1]) }
+      vars.each_pair { |k,v| output += template.dup.gsub!(k,v[index-1]) }
     end
     output
   end
 
-  def debug
-    puts "="*40
-    puts @output
-    puts "="*40
+  def read_rows_from(data_string)
+    puts data_string
+    data = "<template>\n#{data_string}\n</template>"
+    xml = REXML::Document.new(data)
+    xml.elements.each do |i|
+      if i.name == 'row'
+        puts "OK"+i.to_s
+      end
+    end
+    []
   end
-
 end
