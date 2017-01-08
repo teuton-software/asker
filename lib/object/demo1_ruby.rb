4c2628a2a1f8b504d2a5e7f9469e925826640b04
David Vargas <davidvargas.tenerife@gmail.com>
2017-01-08 19:24:04 +0000

    U temp loader for data objects

--- lib/object/demo1_ruby.rb
+++ lib/object/demo1_ruby.rb
@@ -1,26 +1,11 @@
 #!/usr/bin/ruby
 
-require_relative 'data_object'
-require_relative '../formatter/question_gift_formatter'
+require_relative 'object_loader'
 
 files = ['data_array.rb','data_string1.rb','data_string2.rb', 'iterador1.rb']
-questions = {}
+dirbase = File.join('input','files','ruby')
+type = :rubycode
 
-files.each do |filename|
-  filepath = File.join('input','files','ruby',filename)
-  data = DataObject.new(filepath, :rubycode)
-  questions[filename] = data.make_questions
-end
-
-t = 0
-puts '//' + '=' * 80
-questions.each_pair do |k, v|
-  puts "// #{k} #{v.size}"
-  t += v.size
-end
-puts "// TOTAL = #{t}"
-puts '//' + '=' * 80
-
-questions.each_value do |values|
-  values.each { |question| puts QuestionGiftFormatter.new(question).to_s }
-end
+obj = ObjectLoader.new(dirbase, files, type)
+obj.load
+obj.show
---
+++ lib/object/object_loader.rb
@@ -0,0 +1,35 @@
+
+require_relative 'data_object'
+require_relative '../formatter/question_gift_formatter'
+
+class ObjectLoader
+  def initialize(dirbase, files, type)
+    @files = files
+    @dirbase = dirbase
+    @type = type
+    @questions = {}
+  end
+
+  def load
+    @files.each do |filename|
+      filepath = File.join(@dirbase, filename)
+      data = DataObject.new(filepath, @type)
+      @questions[filename] = data.make_questions
+    end
+  end
+
+  def show
+    t = 0
+    puts '//' + '=' * 80
+    @questions.each_pair do |k, v|
+      puts "// #{k} #{v.size}"
+      t += v.size
+    end
+    puts "// TOTAL = #{t}"
+    puts '//' + '=' * 80
+
+    @questions.each_value do |values|
+      values.each { |question| puts QuestionGiftFormatter.new(question).to_s }
+    end
+  end
+end
