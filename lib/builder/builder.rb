06c5356e8fd92743927f480656a210e0cc285675
David Vargas <davidvargas.tenerife@gmail.com>
2017-06-13 19:52:58 +0100

    U reduce rubocop offenses

--- lib/builder/builder.rb
+++ lib/builder/builder.rb
@@ -3,18 +3,17 @@
 require 'rainbow'
 require_relative '../project'
 
+# Define methods used to build new project skeleton
 module Builder
-
   def self.create_project(projectname)
-
     puts "\n[INFO] Creating project <#{Rainbow(projectname).bright}>"
-    projectdir="projects/#{projectname}"
-    Builder::create_dir(projectdir)
+    projectdir = File.join('projects', projectname)
+    Builder.create_dir(projectdir)
 
-    filename=projectdir+"/config.yaml"
-    if !File.exists? filename
+    filename = File.join(projectdir, 'config.yaml')
+    if !File.exist? filename
       puts "* Creating file      => #{Rainbow(filename).color(:green)}"
-      f=File.new(filename,'w')
+      f = File.new(filename, 'w')
       f.write("---\n")
       f.write(":inputdirs: '#{Project.instance.inputbasedir}/#{projectname}'\n")
       f.write(":process_file: '#{projectname}.haml'\n")
@@ -24,14 +23,14 @@ module Builder
       puts "* Exists file! => #{Rainbow(filename).color(:yellow)}"
     end
 
-    inputdir="#{Project.instance.inputbasedir}/#{projectname}"
-    Builder::create_dir(inputdir)
-    filename=inputdir+"/"+projectname+".haml"
-    Builder::create_hamlfile(filename)
+    inputdir = "#{Project.instance.inputbasedir}/#{projectname}"
+    Builder.create_dir(inputdir)
+    filename = File.join(inputdir, projectname + '.haml')
+    Builder.create_hamlfile(filename)
   end
 
   def self.create_dir(dirname)
-    if !Dir.exists? dirname
+    if !Dir.exist? dirname
       puts "* Creating directory => #{Rainbow(dirname).color(:green)}"
       Dir.mkdir(dirname)
     else
@@ -40,13 +39,12 @@ module Builder
   end
 
   def self.create_hamlfile(filename)
-    source = File.join( File.dirname(__FILE__), "sample.haml")
-    if !File.exists? filename
+    source = File.join(File.dirname(__FILE__), 'sample.haml')
+    if !File.exist? filename
       puts "* Creating file      => #{Rainbow(filename).color(:green)}"
-      FileUtils.cp( source, filename)
+      FileUtils.cp(source, filename)
     else
       puts "* Exists file!       => #{Rainbow(dest).color(:yellow)}"
     end
   end
-
 end
