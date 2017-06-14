eb6aa86ed0ee25249f0174cb4cdaae3fe628099d
David Vargas <davidvargas.tenerife@gmail.com>
2017-06-14 19:49:42 +0100

    U reduce rubocop offenses

--- lib/project.rb
+++ lib/project.rb
@@ -34,24 +34,24 @@ class Project
     return @default[key]
   end
 
-  def set(key,value)
-    @param[key]=value
+  def set(key, value)
+    @param[key] = value
   end
 
   def open
-    #We need at least process_file and inputdirs params
-    ext = ".haml"
+    # We need at least process_file and inputdirs params
+    ext = '.haml'
     @param[:process_file] = @param[:process_file] || get(:projectdir).split(File::SEPARATOR).last + ext
-    @param[:projectname]  = @param[:projectname]  || File.basename( @param[:process_file], ext)
-    @param[:inputdirs]    = @param[:inputdirs]    || File.join( get(:inputbasedir), @param[:projectdir] )
+    @param[:projectname] = @param[:projectname] || File.basename(@param[:process_file], ext)
+    @param[:inputdirs] = @param[:inputdirs] || File.join(get(:inputbasedir), @param[:projectdir])
 
-    @param[:logname]      = @param[:logname]      || "#{@param[:projectname]}-log.txt"
-    @param[:outputname]   = @param[:outputname]   || "#{@param[:projectname]}-gift.txt"
-    @param[:lessonname]   = @param[:lessonname]   || "#{@param[:projectname]}-doc.txt"
+    @param[:logname] = @param[:logname] || "#{@param[:projectname]}-log.txt"
+    @param[:outputname] = @param[:outputname] || "#{@param[:projectname]}-gift.txt"
+    @param[:lessonname] = @param[:lessonname] || "#{@param[:projectname]}-doc.txt"
 
-    @param[:logpath]      = @param[:logpath]      || File.join( get(:outputdir), @param[:logname] )
-    @param[:outputpath]   = @param[:outputpath]   || File.join( get(:outputdir), @param[:outputname] )
-    @param[:lessonpath]   = @param[:lessonpath]   || File.join( get(:outputdir), @param[:lessonname] )
+    @param[:logpath] = @param[:logpath] || File.join(get(:outputdir), @param[:logname])
+    @param[:outputpath] = @param[:outputpath] || File.join(get(:outputdir), @param[:outputname])
+    @param[:lessonpath] = @param[:lessonpath] || File.join(get(:outputdir), @param[:lessonname])
 
     create_log_file
     create_output_file
@@ -65,56 +65,60 @@ class Project
   end
 
   def verbose(p_text)
-    l_text = StringColorFilter.filter(p_text)
-    puts l_text if get(:verbose)
-    get(:logfile).write(l_text.to_s + "\n") if get(:logfile)
+    text = StringColorFilter.filter(p_text)
+    puts text if get(:verbose)
+    get(:logfile).write(text.to_s + "\n") if get(:logfile)
   end
 
-  def verboseln(lsText)
-    verbose(lsText + "\n")
+  def verboseln(text)
+    verbose(text + "\n")
   end
 
-private
+  private
+
   def create_log_file
-    #create or reset logfile
-    Dir.mkdir( get(:outputdir) ) if !Dir.exists?( get(:outputdir) )
+    # create or reset logfile
+    Dir.mkdir(get(:outputdir)) unless Dir.exist?(get(:outputdir))
 
-    @param[:logfile] = File.open( get(:logpath),'w')
+    @param[:logfile] = File.open(get(:logpath), 'w')
     f = get(:logfile)
-    f.write("="*50+"\n")
-    f.write("Created by : #{Application::name} (version #{Application::version})\n")
+    f.write('=' * 50 + "\n")
+    f.write("Created by : #{Application.name}")
+    f.write(" (version #{Application.version})\n")
     f.write("File       : #{get(:logname)}\n")
-    f.write("Time       : "+Time.new.to_s+"\n")
+    f.write("Time       : #{Time.new}\n")
     f.write("Author     : David Vargas\n")
-    f.write("="*50+"\n\n")
+    f.write('=' * 50 + "\n\n")
 
-    verbose "[INFO] Project open"
-    verbose "   ├── inputdirs    = " + Rainbow( get(:inputdirs) ).bright
-    verbose "   └── process_file = " + Rainbow( get(:process_file) ).bright
+    verbose '[INFO] Project open'
+    verbose '   ├── inputdirs    = ' + Rainbow(get(:inputdirs)).bright
+    verbose '   └── process_file = ' + Rainbow(get(:process_file)).bright
   end
 
   def create_output_file
-    #Create or reset output file
-    Dir.mkdir( get(:outputdir) ) if !Dir.exists? get(:outputdir)
+    # Create or reset output file
+    Dir.mkdir(get(:outputdir)) if !Dir.exist? get(:outputdir)
 
-    @param[:outputfile] = File.open( get(:outputpath),'w')
+    @param[:outputfile] = File.open(get(:outputpath), 'w')
     f = get(:outputfile)
-    f.write("// "+("="*50)+"\n")
-    f.write("// Created by : #{Application::name} (version #{Application::version})\n")
-    f.write("// File       : #{ get(:outputname) }\n")
-    f.write("// Time       : "+Time.new.to_s+"\n")
+    f.write('// ' + ('=' * 50) + "\n")
+    f.write("// Created by : #{Application.name}")
+    f.write(" (version #{Application.version})\n")
+    f.write("// File       : #{get(:outputname)}\n")
+    f.write("// Time       : #{Time.new}\n")
     f.write("// Author     : David Vargas\n")
-    f.write("// "+("="*50)+"\n")
+    f.write('// ' + ('=' * 50) + "\n")
     f.write("\n")
-    f.write("$CATEGORY: $course$/#{ get(:category).to_s}\n") if get(:category)!=:none
+    f.write("$CATEGORY: $course$/#{get(:category)}\n") if get(:category) != :none
   end
 
   def create_lesson_file
     # Create or reset lesson file
-    @param[:lessonfile] = File.new( get(:lessonpath),'w')
+    @param[:lessonfile] = File.new(get(:lessonpath), 'w')
     f = get(:lessonfile)
     f.write('=' * 50 + "\n")
-    f.write("Created by : #{Application.name} (version #{Application.version})\n")
+    f.write("Created by : #{Application.name}")
+    f.write(" (version #{Application.version})\n")
     f.write("File       : #{get(:lessonname)}\n")
     f.write("Time       : #{Time.new}\n")
     f.write("Author     : David Vargas\n")
