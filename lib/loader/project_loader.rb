d4b2341b6517d8f5776ebd32ed1e415c3455a1a5
David Vargas <davidvargas.tenerife@gmail.com>
2017-06-15 17:33:00 +0100

    U better test

--- lib/loader/project_loader.rb
+++ lib/loader/project_loader.rb
@@ -2,43 +2,47 @@
 require 'yaml'
 require_relative '../project'
 
+# Load params into Project class using arg input
 module ProjectLoader
   def self.load(pArgs = {})
     project = Project.instance
 
     if pArgs.class == Hash
       project.param.merge!(pArgs)
-    elsif pArgs.class == String
-      unless File.exist?(pArgs)
-        msg = Rainbow('[WARN] ProjectLoader.load: ').yellow
-        msg += Rainbow(pArgs).yellow.bright + Rainbow(" dosn't exists!").yellow
-        project.verbose msg
-        exit 1
-      end
+      return true
+    end
 
-      if File.extname(pArgs) == '.haml' || File.extname(pArgs) == '.xml'
-        project.set(:inputdirs, File.dirname(pArgs))
-        project.set(:process_file, File.basename(pArgs))
-      elsif File.extname(pArgs) == '.yaml'
-        project.param.merge!(YAML::load(File.open(pArgs)))
-        project.set(:configfilename, pArgs)
-        project.set(:projectdir, File.dirname(pArgs))
-      elsif File.directory?(pArgs)
-        msg = Rainbow('[WARN] ProjectLoader.load: Directory input ').yellow
-        msg += Rainbow(pArgs).bright.yellow
-        msg += Rainbow(' not implemented yet').yellow
-        project.verbose msg
-        exit 1
-      else
-        msg = Rainbow('[ERR ] ProjectLoader: Input ').red
-        msg += Rainbow(pArgs).red.bright + Rainbow(" unkown").red
-        project.verbose msg
-        exit 1
-      end
-    else
-      msg = Rainbow(format("[ERR ] ProjectLoader: Configuration params format is <%s!>", pArgs.class)).red
-      project.verbose msg
+    if pArgs.class != String
+      msg = "[ERR ] ProjectLoader: Configuration params format is <#{pArgs.class}>!"
+      project.verbose Rainbow(msg).red
       exit
+    end
+
+    unless File.exist?(pArgs)
+      msg = Rainbow('[WARN] ProjectLoader.load: ').yellow
+      msg += Rainbow(pArgs).yellow.bright + Rainbow(" dosn't exists!").yellow
+      project.verbose msg
+      exit 1
+    end
+
+    if File.extname(pArgs) == '.haml' || File.extname(pArgs) == '.xml'
+      project.set(:inputdirs, File.dirname(pArgs))
+      project.set(:process_file, File.basename(pArgs))
+    elsif File.extname(pArgs) == '.yaml'
+      project.param.merge!(YAML.load(File.open(pArgs)))
+      project.set(:configfilename, pArgs)
+      project.set(:projectdir, File.dirname(pArgs))
+    elsif File.directory?(pArgs)
+      msg = Rainbow('[WARN] ProjectLoader.load: Directory input ').yellow
+      msg += Rainbow(pArgs).bright.yellow
+      msg += Rainbow(' not implemented yet').yellow
+      project.verbose msg
+      exit 1
+    else
+      msg = Rainbow('[ERR ] ProjectLoader: Input ').red
+      msg += Rainbow(pArgs).red.bright + Rainbow(' unkown').red
+      project.verbose msg
+      exit 1
     end
   end
 end
--- tests/loader/project_loader_test.rb
+++ tests/loader/project_loader_test.rb
@@ -4,30 +4,41 @@ require 'minitest/autorun'
 require_relative '../../lib/loader/project_loader'
 
 class ProjectLoaderTest < Minitest::Test
-
-  def setup
-    @args  = ['tests/input/jedi.haml', 'tests/input/sith.haml']
-  end
-
-  def test_load_jedi
+  def test_load_jedi_haml
+    filepath = 'tests/input/jedi.haml'
     project = Project.instance
     project.reset
 
     assert_equal 0, project.param.size
-    ProjectLoader.load(@args[0])
+    ProjectLoader.load(filepath)
     assert_equal 2, project.param.size
     assert_equal 'tests/input', project.param[:inputdirs]
     assert_equal 'jedi.haml', project.param[:process_file]
   end
 
-  def test_load_sith
+  def test_load_sith_haml
+    filepath = 'tests/input/sith.haml'
     project = Project.instance
     project.reset
 
     assert_equal 0, project.param.size
-    ProjectLoader.load(@args[1])
+    ProjectLoader.load(filepath)
     assert_equal 2, project.param.size
     assert_equal 'tests/input', project.param[:inputdirs]
     assert_equal 'sith.haml', project.param[:process_file]
+  end
+
+  def test_load_project1_yaml
+    filepath = 'tests/input/project1.yaml'
+    project = Project.instance
+    project.reset
+
+    assert_equal 0, project.param.size
+    ProjectLoader.load(filepath)
+    assert_equal 4, project.param.size
+    assert_equal 'input/es/add,input/es/idp', project.param[:inputdirs]
+    assert_equal 'acceso-remoto.haml', project.param[:process_file]
+    assert_equal 'tests/input/project1.yaml', project.param[:configfilename]
+    assert_equal 'tests/input', project.param[:projectdir]
   end
 end
