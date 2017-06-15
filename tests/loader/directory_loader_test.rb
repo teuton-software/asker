6cb5bf391bc865a95c06793a1019cb2c58b816aa
David Vargas <davidvargas.tenerife@gmail.com>
2017-06-15 20:16:25 +0100

    N DirectoryLoader test

---
+++ tests/loader/directory_loader_test.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/ruby
+
+require 'minitest/autorun'
+require_relative '../../lib/loader/project_loader'
+require_relative '../../lib/loader/directory_loader'
+
+class DirectoryLoaderTest < Minitest::Test
+  def test_load_test_input
+    filepath = 'tests/input/jedi.haml'
+    project = Project.instance
+    project.reset
+    project.set(:verbose, false)
+    ProjectLoader.load(filepath)
+
+    data = DirectoryLoader.load 'tests/input'
+
+    assert_equal 4, data[:concepts].size
+    assert_equal 'obiwan', data[:concepts][0].name
+    assert_equal 'yoda', data[:concepts][1].name
+    assert_equal 'sidious', data[:concepts][2].name
+    assert_equal 'maul', data[:concepts][3].name
+
+    assert_equal true, data[:concepts][0].process?
+    assert_equal true, data[:concepts][1].process?
+    assert_equal false, data[:concepts][2].process?
+    assert_equal false, data[:concepts][3].process?
+    assert_equal 0, data[:fobs].size
+
+    project.reset
+  end
+end
