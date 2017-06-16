f96750aec7eef3f98650dc7d5679527ef1bf268f
David Vargas <davidvargas.tenerife@gmail.com>
2017-06-16 14:01:46 +0100

    U reduce rubocop offenses

--- lib/exporter/concept_screen_exporter.rb
+++ lib/exporter/concept_screen_exporter.rb
@@ -1,22 +1,25 @@
 
 require_relative '../formatter/concept_string_formatter'
 
+# Show Concept Data on screen
 module ConceptScreenExporter
   def self.export(concepts)
-	  project=Project.instance
-    return if project.show_mode==:none
-    project.verbose "\n[INFO] Showing concept data <#{Rainbow(project.show_mode.to_s).bright}>"
+    project = Project.instance
+    return if project.show_mode == :none
+    msg = "\n[INFO] Showing concept data <"
+    msg += Rainbow(project.show_mode.to_s).bright + '>'
+    project.verbose msg
 
     case project.show_mode
     when :resume
-	    s="* Concepts (" + concepts.count.to_s+"): "
-	    concepts.each { |c| s=s+c.name+", " }
-	    project.verbose s
+      s = "* Concepts (#{concepts.count}): "
+      concepts.each { |c| s += c.name + ', ' }
+      project.verbose s
     when :default
       # Only show Concepts with process attr true
-	    concepts.each do |c|
+      concepts.each do |c|
         project.verbose ConceptStringFormatter.new(c).to_s if c.process?
       end
-	  end
+    end
   end
 end
