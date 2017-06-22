234789b41cba48bd16acfcd32326e244ade167f3
David Vargas <davidvargas.tenerife@gmail.com>
2017-06-22 10:51:28 +0100

    U

---
+++ lib/fob/demo4_python.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/ruby
+
+require_relative 'demo_loader'
+
+#files = [,'data_string1.rb',, 'iterador1.rb']
+files = ['string.py', 'array.py', 'iterador.py']
+dirbase = File.join('tests','input','files')
+type = :python
+
+obj = DemoLoader.new(dirbase, files, type)
+obj.load
+obj.show
---
+++ tests/input/files/array.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+nombres = [ "David","Jesua","Bruno", "Daniel" ]
+
+print "Hola " + nombres[0] + "!"
+print "Hola " + nombres[1] + "!"
+print "Hola " + nombres[2] + "!"
+print "Hola " + nombres[3] + "!"
