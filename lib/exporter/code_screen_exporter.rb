eacccb1bb047e3e406b44e11a9cfdec5951fac50
David Vargas <davidvargas.tenerife@gmail.com>
2017-07-10 12:21:32 +0100

    U only show when exists codes

--- lib/exporter/code_screen_exporter.rb
+++ lib/exporter/code_screen_exporter.rb
@@ -2,36 +2,39 @@
 require 'terminal-table'
 
 module CodeScreenExporter
-  def self.export(fobs)
+  def self.export(codes)
 	  project = Project.instance
-    return if project.show_mode == :none || fobs.nil? || fobs.size == 0
+    return if project.show_mode == :none || codes.nil? || codes.size == 0
 
-    total_f = total_q = total_e = 0
+    total_c = total_q = total_e = 0
 
     my_screen_table = Terminal::Table.new do |st|
       st << ['Filename','Type','Questions','Lines','xFactor']
       st << :separator
     end
 
-    fobs.each do |fob|
-      if fob.process?
-        e = fob.lines.size
-        q = fob.questions.size
+    codes.each do |code|
+      if code.process?
+        e = code.lines.size
+        q = code.questions.size
 
         if e == 0 then
           factor = 'Unkown'
         else
           factor = (q.to_f/e.to_f).round(2).to_s
         end
-        my_screen_table.add_row [Rainbow(File.basename(fob.filename)).green, fob.type, q, e, factor]
+        my_screen_table.add_row [Rainbow(File.basename(code.filename)).green, code.type, q, e, factor]
 
-        total_f += 1; total_q += q; total_e += e
+        total_c += 1; total_q += q; total_e += e
       end
     end
 
     my_screen_table.add_separator
-    my_screen_table.add_row [Rainbow("TOTAL = #{total_f.to_s}").bright, ' ', Rainbow(total_q.to_s).bright, Rainbow(total_e.to_s).bright,Rainbow((total_q.to_f/total_e.to_f).round(2)).bright]
-    project.verbose "\n[INFO] Showing CODE statistics\n"
-    project.verbose my_screen_table.to_s + "\n"
+    my_screen_table.add_row [Rainbow("TOTAL = #{total_c.to_s}").bright, ' ', Rainbow(total_q.to_s).bright, Rainbow(total_e.to_s).bright,Rainbow((total_q.to_f/total_e.to_f).round(2)).bright]
+
+    if total_c > 0
+      project.verboseln "\n[INFO] Showing CODE statistics"
+      project.verboseln my_screen_table.to_s
+    end
   end
 end
