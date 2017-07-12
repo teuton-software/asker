39a5baed479d799e8bbbe21bf5f3f5a8679bf8f5
David Vargas <davidvargas.tenerife@gmail.com>
2017-07-12 21:19:12 +0100

    U more tests

--- tests/formatter/question_gift_formatter_test.rb
+++ tests/formatter/question_gift_formatter_test.rb
@@ -33,7 +33,14 @@ class QuestionGiftFormatterTest < Minitest::Test
     q3.matching << [ 'a4', 'b4' ]
     q3.feedback = 'match feedback'
 
-    @questions = [ q0, q1, q2, q3 ]
+    q4 = Question.new(:short)
+    q4.name = 'my name 4'
+    q4.comment = 'short comment 4'
+    q4.text = 'my text 4'
+    q4.shorts << 'OK1'
+    q4.feedback = 'short feedback'
+
+    @questions = [q0, q1, q2, q3, q4]
   end
 
   def test_boolean_question_formatter
@@ -48,6 +55,18 @@ class QuestionGiftFormatterTest < Minitest::Test
     assert_equal c, line[1]
     c = format("{%s####%s}", question.good, question.feedback)
     assert_equal c, line[2]
+
+    question.feedback = nil
+    a = QuestionGiftFormatter.to_s(question)
+    line = a.split("\n")
+
+    c = format("// %s", question.comment)
+    assert_equal c, line[0]
+    c = format("::%s::[html]%s", question.name, question.text)
+    assert_equal c, line[1]
+    c = format("{%s####}", question.good)
+    assert_equal c, line[2]
+
   end
 
   def test_choice_question_formatter
@@ -74,6 +93,26 @@ class QuestionGiftFormatterTest < Minitest::Test
     c = format("  ####%s", question.feedback)
     assert_equal c, line[7]
     assert_equal '}', line[8]
+
+    question.feedback = nil
+    a = QuestionGiftFormatter.to_s(question)
+    line = a.split("\n")
+
+    c = format("// %s", question.comment)
+    assert_equal c, line[0]
+    c = format("::%s::[html]%s", question.name, question.text)
+    assert_equal c, line[1]
+    assert_equal '{', line[2]
+    d = [line[3], line[4], line[5], line[6]]
+    d.sort!
+    assert_equal format("  =%s", question.good), d[0]
+    c = '  ~%-25%' + question.bads[0]
+    assert_equal c, d[1]
+    c = '  ~%-25%' + question.bads[1]
+    assert_equal c, d[2]
+    c = '  ~%-25%' + question.bads[2]
+    assert_equal c, d[3]
+    assert_equal '}', line[7]
   end
 
   def test_match_question_formatter
@@ -98,5 +137,36 @@ class QuestionGiftFormatterTest < Minitest::Test
     m = question.matching[3]
     assert_equal format("  =%s -> %s", m[0], m[1]), d[3]
     assert_equal '}', line[7]
+  end
+
+  def test_short_question_formatter
+    index = 4
+    question = @questions[index]
+    a = QuestionGiftFormatter.to_s(question)
+    line = a.split("\n")
+
+    c = format("// %s", question.comment)
+    assert_equal c, line[0]
+    c = format("::%s::[html]%s", question.name, question.text)
+    assert_equal c, line[1]
+    assert_equal '{', line[2]
+    m = question.shorts[0]
+    c = '  =%100%' + m + '#'
+    assert_equal c, line[3]
+    assert_equal format("  ####%s", question.feedback), line[4]
+    assert_equal '}', line[5]
+
+    question.feedback = nil
+    a = QuestionGiftFormatter.to_s(question)
+    line = a.split("\n")
+
+    c = format("// %s", question.comment)
+    assert_equal c, line[0]
+    c = format("::%s::[html]%s", question.name, question.text)
+    assert_equal c, line[1]
+    assert_equal '{', line[2]
+    m = question.shorts[0]
+    c = '  =%100%' + m + '#'
+    assert_equal '}', line[4]
   end
 end
