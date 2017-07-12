f4134ce582488a8e3258908290825f595c9f89a8
David Vargas <davidvargas.tenerife@gmail.com>
2017-07-12 21:08:42 +0100

    N test

--- tests/all.rb
+++ tests/all.rb
@@ -8,9 +8,6 @@ require_relative 'project_test'
 require_relative 'lang/lang_test'
 require_relative 'lang/lang_factory_test'
 
-require_relative 'formatter/string_color_filter_test'
-#require_relative 'formatter/concept_string_formatter_test'
-
 require_relative 'loader/image_url_loader_test'
 require_relative 'loader/file_loader_test'
 require_relative 'loader/directory_loader_test'
@@ -27,9 +24,12 @@ require_relative 'data/table_test'
 require_relative 'data/template_test'
 require_relative 'data/row_test'
 
+require_relative 'formatter/string_color_filter_test'
+#require_relative 'formatter/concept_string_formatter_test'
+require_relative 'formatter/table_haml_formatter_test'
+require_relative 'formatter/concept_haml_formatter_test'
+require_relative 'formatter/question_gift_formatter_test'
+
 require_relative 'ai/question_test'
 require_relative 'ai/concept_ai_test'
 require_relative 'ai/stages/base_stage_test'
-
-require_relative 'formatter/table_haml_formatter_test'
-require_relative 'formatter/concept_haml_formatter_test'
---
+++ tests/formatter/question_gift_formatter_test.rb
@@ -0,0 +1,102 @@
+#!/usr/bin/ruby
+
+require 'minitest/autorun'
+require_relative '../../lib/ai/question'
+require_relative '../../lib/formatter/question_gift_formatter'
+
+class QuestionGiftFormatterTest < Minitest::Test
+  def setup
+    q0 = Question.new
+
+    q1 = Question.new(:boolean)
+    q1.name = 'my name 1'
+    q1.comment = 'boolean comment 1'
+    q1.text = 'my text 1'
+    q1.good = true
+    q1.feedback = 'You know is true'
+
+    q2 = Question.new(:choice)
+    q2.name = 'my name 2'
+    q2.comment = 'choice comment 2'
+    q2.text = 'my text 2'
+    q2.good = 'good 2'
+    q2.bads = [ 'bad1', 'bad2', 'bad3' ]
+    q2.feedback = '2 is OK'
+
+    q3 = Question.new(:match)
+    q3.name = 'my name 3'
+    q3.comment = 'match comment 3'
+    q3.text = 'my text 3'
+    q3.matching << [ 'a1', 'b1' ]
+    q3.matching << [ 'a2', 'b2' ]
+    q3.matching << [ 'a3', 'b3' ]
+    q3.matching << [ 'a4', 'b4' ]
+    q3.feedback = 'match feedback'
+
+    @questions = [ q0, q1, q2, q3 ]
+  end
+
+  def test_boolean_question_formatter
+    index = 1
+    question = @questions[index]
+    a = QuestionGiftFormatter.to_s(question)
+    line = a.split("\n")
+
+    c = format("// %s", question.comment)
+    assert_equal c, line[0]
+    c = format("::%s::[html]%s", question.name, question.text)
+    assert_equal c, line[1]
+    c = format("{%s####%s}", question.good, question.feedback)
+    assert_equal c, line[2]
+  end
+
+  def test_choice_question_formatter
+    index = 2
+    question = @questions[index]
+    a = QuestionGiftFormatter.to_s(question)
+    line = a.split("\n")
+
+    c = format("// %s", question.comment)
+    assert_equal c, line[0]
+    c = format("::%s::[html]%s", question.name, question.text)
+    assert_equal c, line[1]
+    assert_equal '{', line[2]
+    d = [line[3], line[4], line[5], line[6]]
+    d.sort!
+    assert_equal format("  =%s", question.good), d[0]
+    c = '  ~%-25%' + question.bads[0]
+    assert_equal c, d[1]
+    c = '  ~%-25%' + question.bads[1]
+    assert_equal c, d[2]
+    c = '  ~%-25%' + question.bads[2]
+    assert_equal c, d[3]
+
+    c = format("  ####%s", question.feedback)
+    assert_equal c, line[7]
+    assert_equal '}', line[8]
+  end
+
+  def test_match_question_formatter
+    index = 3
+    question = @questions[index]
+    a = QuestionGiftFormatter.to_s(question)
+    line = a.split("\n")
+
+    c = format("// %s", question.comment)
+    assert_equal c, line[0]
+    c = format("::%s::[html]%s", question.name, question.text)
+    assert_equal c, line[1]
+    assert_equal '{', line[2]
+    d = [line[3], line[4], line[5], line[6]]
+    d.sort!
+    m = question.matching[0]
+    assert_equal format("  =%s -> %s", m[0], m[1]), d[0]
+    m = question.matching[1]
+    assert_equal format("  =%s -> %s", m[0], m[1]), d[1]
+    m = question.matching[2]
+    assert_equal format("  =%s -> %s", m[0], m[1]), d[2]
+    m = question.matching[3]
+    assert_equal format("  =%s -> %s", m[0], m[1]), d[3]
+    assert_equal '}', line[7]
+  end
+end
