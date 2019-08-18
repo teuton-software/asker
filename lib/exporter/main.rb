707e1f3476dbcfaf12947efa8f755ac727aaa4f8
David <dvarrui@protonmail.com>
2019-08-19 00:53:20 +0100

    U refactoring

---
+++ lib/exporter/main.rb
@@ -0,0 +1,8 @@
+
+require_relative 'code_gift_exporter'
+require_relative 'code_screen_exporter'
+
+require_relative 'concept_ai_gift_exporter'
+require_relative 'concept_ai_screen_exporter'
+require_relative 'concept_doc_exporter'
+require_relative 'concept_screen_exporter'
