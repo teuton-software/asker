1f87457535230af8f2062b4e4a6fc178d250fe12
David <dvarrui@protonmail.com>
2019-08-22 15:57:19 +0100

    U

--- lib/loader/file_loader.rb
+++ lib/loader/file_loader.rb
@@ -1,4 +1,4 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
 require 'haml'
 require_relative 'content_loader'
@@ -6,11 +6,8 @@ require_relative 'content_loader'
 # Methods that load a filename and return list of concepts
 module FileLoader
   def self.load(filename)
-
     if File.extname(filename).casecmp('.haml').zero?
-      template = File.read(filename)
-      haml_engine = Haml::Engine.new(template)
-      file_content = haml_engine.render
+      file_content = load_haml filename
     elsif File.extname(filename).casecmp('.xml').zero?
       file_content = open(filename, &:read)
     else
@@ -21,5 +18,11 @@ module FileLoader
 
     data = ContentLoader.new(filename, file_content).load
     data
+  end
+
+  def self.load_haml(filename)
+    template = File.read(filename)
+    haml_engine = Haml::Engine.new(template)
+    haml_engine.render
   end
 end
