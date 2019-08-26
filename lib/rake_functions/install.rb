b8ec89ce4e4fc28aca16048da41b6d510a9c7808
David <dvarrui@protonmail.com>
2019-08-26 13:54:35 +0100

    U --no-ri for gnulinux intallations

--- lib/rake_functions/install.rb
+++ lib/rake_functions/install.rb
@@ -3,7 +3,7 @@
 
 desc 'OpenSUSE installation'
 task :opensuse => :gems do
-  install_gems packages
+  install_gems packages, '--no-ri'
   create_symbolic_link
   puts "[INFO] Run 'asker download' to download sample input files from repo"
 end
@@ -13,7 +13,7 @@ task :debian do
   names = ['make', 'gcc', 'build-essential', 'ruby-dev']
   names.each { |name| system("apt install -y #{name}") }
 
-  install_gems packages
+  install_gems packages,'--no-ri'
   create_symbolic_link
   puts "[INFO] Run 'asker download' to download sample input files from repo"
 end
@@ -23,8 +23,8 @@ task :gems do
   install_gems packages
 end
 
-def install_gems(list)
+def install_gems(list, options = '')
   puts "[INFO] Installing Ruby gems..."
   fails = filter_uninstalled_gems(list)
-  fails.each { |name| system("gem install #{name}") }
+  fails.each { |name| system("gem install #{name} #{options}") }
 end
