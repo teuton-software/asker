d0f2aa18186e58bc61f1611fa97a3b7abfa8b2cb
David <dvarrui@protonmail.com>
2019-08-28 23:15:30 +0100

    N N hash formatter

---
+++ lib/formatter/question_hash_formatter.rb
@@ -0,0 +1,40 @@
+
+# Transform Questions into YAML format
+module QuestionHashFormatter
+  def self.to_s(question)
+    @question = question
+    # Return question using YAML format
+    s = {}
+    s[:comment] = @question.comment
+    s[:name] = @question.name
+    s[:text] = sanitize(@question.text)
+    s[:type] = @question.type
+    s[:feedback] = sanitize(@question.feedback.to_s)
+
+    case @question.type
+    when :choice
+      s[:answer] = sanitize(@question.good)
+      s[:options] = (@question.bads + [@question.good]).shuffle
+    when :boolean
+      s[:answer] = @question.good
+    when :match
+      s[:answer] = @question.matching
+      s[:matching] = @question.matching
+    when :short
+      @question.shorts.uniq!
+      s[:answer] = @question.shorts
+    end
+    s.to_s
+  end
+
+  def self.sanitize(input = '')
+    output = input.dup
+    output.gsub!("#", "\\#")
+    output.gsub!("\n", " ")
+    #output.gsub!(":", "\\:")
+    output.gsub!("=", "\\=")
+    output.gsub!("\{", "\\{")
+    output.gsub!("\}", "\\}")
+    output
+  end
+end
