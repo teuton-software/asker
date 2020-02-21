11afaa4910b60602c1d74d95356ffc564c15525d
dvarrui <dvarrui@protonmail.com>
2020-02-21 20:47:01 +0000

    U

--- tasks/install.rb
+++ tasks/install.rb
@@ -2,11 +2,17 @@
 require_relative 'packages'
 
 namespace :install do
+  desc 'Install gems'
+  task :gems do
+    install_gems packages
+  end
+
   desc 'OpenSUSE installation'
   task :opensuse do
     names = ['ruby-devel']
     names.each { |name| system("zypper -y #{name}") }
-    install_gems packages, '--no-ri'
+    Rake::Task['install:gems'].invoke
+    Rake::Task['install:devel'].invoke
     create_symbolic_link
   end
 
@@ -14,31 +20,27 @@ namespace :install do
   task :debian do
     names = ['make', 'gcc', 'build-essential', 'ruby-dev']
     names.each { |name| system("apt install -y #{name}") }
-    install_gems packages,'--no-ri'
+    Rake::Task['install:gems'].invoke
+    Rake::Task['install:devel'].invoke
     create_symbolic_link
-  end
-
-  desc 'Install gems'
-  task :gems do
-    install_gems packages
-  end
-
-  def install_gems(list, options = '')
-    puts "[INFO] Installing Ruby gems (options=#{options})..."
-    fails = filter_uninstalled_gems(list)
-    fails.each { |name| system("gem install #{name} #{options}") }
   end
 
   desc 'Install developer gems'
   task :devel do
     puts "[INFO] Installing developer Ruby gems..."
     p = %w[rubocop minitest pry-byebug yard]
-    p.each { |name| system("gem install #{name}") }
+    #p + %w[sinatra base64_compatible coderay ]
+    install_gems p,'--no-ri'
+  end
+
+  def install_gems(list)
+    puts "[INFO] Installing Ruby gems..."
+    fails = filter_uninstalled_gems(list)
+    fails.each { |name| system("gem install #{name}") }
   end
 
   def create_symbolic_link
     puts '[INFO] Creating symbolic link into /usr/local/bin'
-    basedir = File.dirname(__FILE__)
-    system("ln -s #{basedir}/asker /usr/local/bin/asker")
+    system("ln -s #{Dir.pwd}/asker /usr/local/bin/asker")
   end
 end
