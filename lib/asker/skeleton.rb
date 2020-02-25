973e8cb11af39f71176a26ecb8908196f8d09514
dvarrui <dvarrui@protonmail.com>
2020-02-25 16:10:56 +0000

    U

--- lib/asker/skeleton.rb
+++ lib/asker/skeleton.rb
@@ -3,10 +3,9 @@
 require 'fileutils'
 require 'rainbow'
 
-# Project#create
+# Skeleton: create skeleton for asker input files
 # * create
 # * create_main_dir_and_files
-# * create_assets_dir_and_files
 # * create_dir
 # * create_dirs
 # * copyfile
@@ -14,17 +13,17 @@ module Skeleton
   def self.create(project_dir)
     project_name = File.basename(project_dir)
     puts "\n[INFO] Creating #{Rainbow(project_name).bright} project skeleton"
-    source_basedir = File.join(File.dirname(__FILE__))
     create_dir project_dir
-    create_main_dir_and_files(project_dir, source_basedir)
+    copy_files_into(project_dir)
   end
 
-  def self.create_main_dir_and_files(project_dir, source_basedir)
+  def self.copy_files_into(project_dir)
     # Directory and files: Ruby script, Configfile, gitignore
     items = [
       { source: 'files/example-concept.haml', target: 'example-concept.yaml' },
       { source: 'files/example-code.haml', target: 'example-code.haml.rb' },
     ]
+    source_basedir = File.join(File.dirname(__FILE__))
     items.each do |item|
       source = File.join(source_basedir, item[:source])
       target = File.join(project_dir, item[:target])
