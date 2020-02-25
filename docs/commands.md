d657d5eb1a99cfe71ec391ad7fd5888f2073ec83
dvarrui <dvarrui@protonmail.com>
2020-02-25 16:02:42 +0000

    N skeleton...

--- docs/commands.md
+++ docs/commands.md
@@ -3,13 +3,10 @@
 
 # Commands
 
-By now, ASKER run as CLI command.
-
-| Command                 | Description                     |
-| ----------------------- | ------------------------------- |
-| asker                   | Show available functions        |
-| asker version           | Show current version            |
-| asker PATH/TO/INPUTFILE | Create questions for input file |
-| asker file PATH/TO/INPUTFILE | File keyword is optional   |
-| asker check PATH/TO/INPUTFILE | Check HAML file syntax    |
-| asker init              | Create default config.ini file  |
+Running `asker` as CLI command with these available command functions:
+* `asker`: Show available functions.
+* `asker version`: Show current version.
+* `asker PATH/TO/INPUTFILE`: Create questions for selected input file.
+* `asker file PATH/TO/INPUTFILE`: It's the same as `asker PATH/TO/INPUTFILE`, because `file` keyword is optional.
+* `asker check PATH/TO/INPUTFILE`: Check HAML file syntax.
+* `asker init`: Create default config.ini file.
---
+++ lib/asker/skeleton.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+require 'fileutils'
+require 'rainbow'
+
+# Project#create
+# * create
+# * create_main_dir_and_files
+# * create_assets_dir_and_files
+# * create_dir
+# * create_dirs
+# * copyfile
+module Skeleton
+  def self.create(project_dir)
+    project_name = File.basename(project_dir)
+    puts "\n[INFO] Creating #{Rainbow(project_name).bright} project skeleton"
+    source_basedir = File.join(File.dirname(__FILE__))
+    create_dir project_dir
+    create_main_dir_and_files(project_dir, source_basedir)
+  end
+
+  def self.create_main_dir_and_files(project_dir, source_basedir)
+    # Directory and files: Ruby script, Configfile, gitignore
+    items = [
+      { source: 'files/example-concept.haml', target: 'example-concept.yaml' },
+      { source: 'files/example-code.haml', target: 'example-code.haml.rb' },
+    ]
+    items.each do |item|
+      source = File.join(source_basedir, item[:source])
+      target = File.join(project_dir, item[:target])
+      copyfile(source, target)
+    end
+  end
+
+  def self.create_dir(dirpath)
+    if Dir.exist? dirpath
+      puts "* Exists dir!       => #{Rainbow(dirpath).yellow}"
+    else
+      begin
+        FileUtils.mkdir_p(dirpath)
+        puts "* Create dir        => #{Rainbow(dirpath).green}"
+      rescue StandardError
+        puts "* Create dir  ERROR => #{Rainbow(dirpath).red}"
+      end
+    end
+  end
+
+  def self.create_dirs(*args)
+    args.each { |arg| create_dir arg }
+  end
+
+  def self.copyfile(target, dest)
+    if File.exist? dest
+      puts "* Exists file!      => #{Rainbow(dest).yellow}"
+    else
+      puts "* File not found!   => #{Rainbow(target).yellow}" unless File.exist? target
+      begin
+        FileUtils.cp(target, dest)
+        puts "* Create file       => #{Rainbow(dest).green}"
+      rescue StandardError
+        puts "* Create file ERROR => #{Rainbow(dest).red}"
+      end
+    end
+  end
+end
