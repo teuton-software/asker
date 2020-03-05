d3e68b3f7ac50758d660e8615c1b50920dc93fee
dvarrui <dvarrui@protonmail.com>
2020-03-05 04:59:11 +0000

    U

--- lib/asker/lang/lang_factory.rb
+++ lib/asker/lang/lang_factory.rb
@@ -10,13 +10,10 @@ class LangFactory
 
   def initialize
     @langs = {}
-    languages = Application.instance.config['languages'].dup
-    languages.delete("default")
-    languages.each_pair do |key, value|
+    Application.instance.config['languages'].each_pair do |key, value|
       next if key == 'default'
       @langs[key] = Lang.new(key) if value.downcase == 'yes'
     end
-#    Project.instance.locales.each { |i| @langs[i] = Lang.new(i) }
   end
 
   def get(locale)
