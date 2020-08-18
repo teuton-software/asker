cae55f614dedcfe25db740d649b6d433ab0f531b
David Vargas <dvarrui@protonmail.com>
2020-08-18 12:17:55 +0100

    N fr code lang

--- lib/asker/lang/lang_factory.rb
+++ lib/asker/lang/lang_factory.rb
@@ -29,7 +29,10 @@ class LangFactory
   # Return Lang object associated to code
   # @param code (String)
   def get(code)
-    @langs[code]
+    return @langs[code] unless @langs[code].nil?
+    puts Rainbow("[ERROR] Unkown Lang code: #{code}").bright
+    puts Rainbow("        Change input file code lang, revise configuration from config.ini and template files.").bright
+    exit 1
   end
 
   ##
