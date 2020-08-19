770c68ceca2a53d115adfacc328ce263447a4a69
David Vargas <dvarrui@protonmail.com>
2020-08-19 16:03:36 +0100

    U

--- lib/asker/formatter/question_moodle_formatter.rb
+++ lib/asker/formatter/question_moodle_formatter.rb
@@ -9,28 +9,19 @@ module QuestionMoodleFormatter
   # @param question (Question)
   # @return String
   def self.to_s(question)
-    # @question.comment.nil? && @question.comment.empty?
-
     case question.type
     when :choice
       penalties = ['', '%-50%', '%-33.33333%', '%-25%', '%-20%']
       penalty = penalties[question.bads.size]
       template = File.read(File.join(File.dirname(__FILE__), 'moodle', 'multichoice.erb'))
-      renderer = ERB.new(template)
-      s = renderer.result(binding)
     when :boolean
       template = File.read(File.join(File.dirname(__FILE__), 'moodle', 'truefalse.erb'))
-      renderer = ERB.new(template)
-      s = renderer.result(binding)
     when :match
       template = File.read(File.join(File.dirname(__FILE__), 'moodle', 'matching.erb'))
-      renderer = ERB.new(template)
-      s = renderer.result(binding)
     when :short
       template = File.read(File.join(File.dirname(__FILE__), 'moodle', 'shortanswer.erb'))
-      renderer = ERB.new(template)
-      s = renderer.result(binding)
     end
-    s
+    renderer = ERB.new(template)
+    renderer.result(binding)
   end
 end
