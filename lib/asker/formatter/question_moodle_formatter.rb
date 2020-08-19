cedfa2d658b81e1d40ae2b2c8a0178d9b9e53c9c
David Vargas <dvarrui@protonmail.com>
2020-08-19 12:24:41 +0100

    U working on moodle xml formatter...

--- lib/asker/formatter/question_moodle_formatter.rb
+++ lib/asker/formatter/question_moodle_formatter.rb
@@ -7,32 +7,21 @@ module QuestionMoodleFormatter
   # @param question (Question)
   # @return String
   def self.to_s(question)
-    @question = question
     # @question.comment.nil? && @question.comment.empty?
 
-    case @question.type
+    case question.type
     when :choice
-      s = @question.name + "\n"
-      #a = ["  =#{sanitize(@question.good)}\n"]
-      #penalties = ['', '%-50%', '%-33.33333%', '%-25%', '%-20%']
-      #penalty = penalties[@question.bads.size]
-
-      #@question.bads.each { |i| a << ("  ~#{penalty}" + sanitize(i) + "\n") }
-      #a.shuffle! if @question.shuffle?
-      #a.each do |i|
-      #  text = i
-      #  text = i[0, 220] + '...(ERROR: text too long)' if text.size > 255
-      #  s << text
-      #end
-      #s += "  #####{sanitize(@question.feedback.to_s)}\n" if @question.feedback
-      #s += "}\n\n"
+      penalties = ['', '%-50%', '%-33.33333%', '%-25%', '%-20%']
+      penalty = penalties[question.bads.size]
+      template = File.read(File.join(File.dirname(__FILE__), 'moodle', 'multichoice.erb'))
+      renderer = ERB.new(template)
+      s = renderer.result(binding)
     when :boolean
-      #s << "{#{@question.good}#####{sanitize(@question.feedback.to_s)}}\n\n"
       template = File.read(File.join(File.dirname(__FILE__), 'moodle', 'truefalse.erb'))
       renderer = ERB.new(template)
       s = renderer.result(binding)
     when :match
-      s = @question.name + "\n"
+      s = question.name + "\n"
       #s << "{\n"
       #a = []
       #@question.matching.each do |i, j|
@@ -44,16 +33,9 @@ module QuestionMoodleFormatter
       #a.each { |i| s << i }
       #s << "}\n\n"
     when :short
-      s = @question.name + "\n"
-      #s << "{\n"
-      #@question.shorts.uniq!
-      #@question.shorts.each do |i|
-      #  text = i
-      #  text = i[0, 220] + '...(ERROR: too long)' if text.size > 255
-      #  s << "  =%100%#{text}#\n"
-      #end
-      #s << "  #####{sanitize(@question.feedback.to_s)}\n" if @question.feedback
-      #s << "}\n\n"
+      template = File.read(File.join(File.dirname(__FILE__), 'moodle', 'shortanswer.erb'))
+      renderer = ERB.new(template)
+      s = renderer.result(binding)
     end
     s
   end
