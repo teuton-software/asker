477e47c22677832cd2d01716c85a863062bf3bd9
David Vargas <dvarrui@protonmail.com>
2020-08-19 18:37:04 +0100

    F

--- lib/asker/ai/stages/stage_b.rb
+++ lib/asker/ai/stages/stage_b.rb
@@ -8,22 +8,22 @@ require_relative '../question'
 class StageB < BaseStage
   # range b1
 
-  def run(pTable, pList1, pList2)
+  def run(table, list1, list2)
     # process table match
     questions = []
-    return questions if pTable.fields.count < 2
+    return questions if table.fields.count < 2
 
     return questions unless type == 'text'
 
-    if pTable.fields.count>1 then
-      questions += process_table_match2fields(pTable, pList1, pList2, 0, 1)
-    elsif pTable.fields.count>2 then
-      questions += process_table_match2fields(pTable, pList1, pList2, 0, 2)
-      questions += process_table_match2fields(pTable, pList1, pList2, 1, 2)
-    elsif pTable.fields.count>3 then
-      questions += process_table_match2fields(pTable, pList1, pList2, 0, 3)
-      questions += process_table_match2fields(pTable, pList1, pList2, 1, 3)
-      questions += process_table_match2fields(pTable, pList1, pList2, 2, 3)
+    if table.fields.count>1 then
+      questions += process_table_match2fields(table, list1, list2, 0, 1)
+    elsif table.fields.count>2 then
+      questions += process_table_match2fields(table, list1, list2, 0, 2)
+      questions += process_table_match2fields(table, list1, list2, 1, 2)
+    elsif table.fields.count>3 then
+      questions += process_table_match2fields(table, list1, list2, 0, 3)
+      questions += process_table_match2fields(table, list1, list2, 1, 3)
+      questions += process_table_match2fields(table, list1, list2, 2, 3)
     end
 
     questions
@@ -36,22 +36,26 @@ class StageB < BaseStage
       pList1.each_cons(4) do |e1,e2,e3,e4|
         e = [ e1, e2, e3, e4 ]
 
-        #Question type <b1match>: match 4 items from the same table
+        # Question type <b1match>: match 4 items from the same table
         e.shuffle!
-        q=Question.new(:match)
-        q.name="#{name}-#{num.to_s}-b1match4x4-#{pTable.name}"
-        q.text= random_image_for(name) + lang.text_for(:b1, name, pTable.fields[pIndex1].capitalize, pTable.fields[pIndex2].capitalize )
+        q = Question.new(:match)
+        q.name = "#{name}-#{num.to_s}-b1match4x4-#{pTable.name}"
+        q.text = random_image_for(name) + lang.text_for(:b1, name, pTable.fields[pIndex1].capitalize, pTable.fields[pIndex2].capitalize )
         q.matching << [ e[0][:data][pIndex1], e[0][:data][pIndex2] ]
         q.matching << [ e[1][:data][pIndex1], e[1][:data][pIndex2] ]
         q.matching << [ e[2][:data][pIndex1], e[2][:data][pIndex2] ]
         q.matching << [ e[3][:data][pIndex1], e[3][:data][pIndex2] ]
+#        if pList2.count > 0
+          # Add an extra line from Table-B
+#          q.matching << [ "", pList2[0][:data][pIndex2] ]
+#        end
         questions << q
 
         # Question type <b1match>: match 3 items from table-A and 1 item with error
         e.shuffle!
-        q=Question.new(:match)
-        q.name="#{name}-#{num.to_s}-b1match3x1misspelled-#{pTable.name}"
-        q.text= random_image_for(name) + lang.text_for(:b1, name, pTable.fields[pIndex1].capitalize, pTable.fields[pIndex2].capitalize )
+        q = Question.new(:match)
+        q.name = "#{name}-#{num.to_s}-b1match3x1misspelled-#{pTable.name}"
+        q.text = random_image_for(name) + lang.text_for(:b1, name, pTable.fields[pIndex1].capitalize, pTable.fields[pIndex2].capitalize )
         q.matching << [ e[0][:data][pIndex1], e[0][:data][pIndex2] ]
         q.matching << [ e[1][:data][pIndex1], e[1][:data][pIndex2] ]
         q.matching << [ e[2][:data][pIndex1], e[2][:data][pIndex2] ]
@@ -61,18 +65,18 @@ class StageB < BaseStage
     end
 
     if pList1.count>2 and pList2.count>0
-      s=Set.new
+      s = Set.new
       pList1.each do |i|
         s.add( i[:data][pIndex1]+"<=>"+i[:data][pIndex2] )
       end
       s.add( pList2[0][:data][pIndex1]+"<=>"+pList2[0][:data][pIndex2] )
-      a=s.to_a
+      a = s.to_a
 
       # Question 3 items from table-A, and 1 item from table-B
       if s.count > 3
-        q=Question.new(:match)
-        q.name="#{name}-#{num.to_s}-b1match3x1-#{pTable.name}"
-        q.text= random_image_for(name) + lang.text_for(:b1, name , pTable.fields[pIndex1].capitalize, pTable.fields[pIndex2].capitalize)
+        q = Question.new(:match)
+        q.name = "#{name}-#{num.to_s}-b1match3x1-#{pTable.name}"
+        q.text = random_image_for(name) + lang.text_for(:b1, name , pTable.fields[pIndex1].capitalize, pTable.fields[pIndex2].capitalize)
         q.matching << [ pList1[0][:data][pIndex1], pList1[0][:data][pIndex2] ]
         q.matching << [ pList1[1][:data][pIndex1], pList1[1][:data][pIndex2] ]
         q.matching << [ pList1[2][:data][pIndex1], pList1[2][:data][pIndex2] ]
