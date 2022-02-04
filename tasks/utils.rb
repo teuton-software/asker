ba8703f97a6dff0e7011725438e85c29b9b52e26
dvarrui <dvarrui@protonmail.com>
2022-02-04 02:19:18 +0000

    U

--- tasks/utils.rb
+++ tasks/utils.rb
@@ -4,7 +4,7 @@
 # Module Utils with functions used by task/install.rb
 module Utils
   def self.packages
-    %w[haml rainbow terminal-table thor inifile pry-byebug]
+    %w[haml inifile pry-byebug rainbow rexml terminal-table thor]
   end
 
   def self.filter_uninstalled_gems(list)
