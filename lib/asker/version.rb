dc911e47d5a97c9986bbf98d4e41806578810b09
David Vargas <dvarrui@protonmail.com>
2022-02-09 12:15:11 +0000

    [F] .

--- lib/asker/version.rb
+++ lib/asker/version.rb
@@ -1,9 +1,9 @@
 
 # Global parameters
 module Version
-  VERSION = '2.2.3'
+  VERSION = '2.2.4'
   NAME = 'asker'
   GEM = 'asker-tool'
   CONFIGFILE = 'asker.ini'
-  HOMEPAGE = "https://github.com/teuton-software/#{NAME}/tree/v2.2"
+  HOMEPAGE = "https://github.com/teuton-software/#{NAME}/tree/v2.2\#readme"
 end
