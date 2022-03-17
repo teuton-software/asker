8a667704cf9e7419a8f358f14e73390210458504
David Vargas <dvarrui@protonmail.com>
2022-03-17 20:28:43 +0000

    [F] .

--- lib/asker/version.rb
+++ lib/asker/version.rb
@@ -2,7 +2,7 @@
 # Global parameters
 module Version
   MAJOR_NUMBER = '2.3'
-  VERSION = "#{MAJOR_NUMBER}.1"
+  VERSION = "#{MAJOR_NUMBER}.2"
   NAME = 'asker'
   GEM = 'asker-tool'
   CONFIGFILE = 'asker.ini'
