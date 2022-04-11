667130fc8e424095cf12868b8c2e68de796bf200
David Vargas <dvarrui@protonmail.com>
2022-04-11 15:44:56 +0100

    [N] .

--- lib/asker/exporter/code_moodle_exporter.rb
+++ lib/asker/exporter/code_moodle_exporter.rb
@@ -3,14 +3,9 @@ require_relative '../formatter/question_moodle_formatter'
 
 module CodeMoodleExporter
 
-  def self.run(codes, file)
-    codes.each { |code| export(code, file) }
-  end
-
-  def self.export(code, file)
+  def self.run(code, file)
     return false unless code.process?
 
-    file.write head(code)
     code.questions.each do |question|
       file.write QuestionMoodleFormatter.to_s(question)
     end
