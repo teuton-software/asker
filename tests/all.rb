fe2f0967d57dc7a9917e88ff45a2bcc6bc3d0663
David Vargas <dvarrui@protonmail.com>
2022-08-10 23:25:32 +0100

    [N] test.

--- tests/all.rb
+++ tests/all.rb
@@ -26,6 +26,7 @@ require_relative 'lang/lang_test'
 require_relative 'lang/lang_factory_test'
 
 #require_relative 'loader/image_url_loader_test'
+require_relative 'loader/embedded_file_test'
 require_relative 'loader/file_loader_test'
 require_relative 'loader/directory_loader_test'
 require_relative 'loader/input_loader_test'
