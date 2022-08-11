08c8b169fc465020ba1404a24a45c8b4997ccf03
David Vargas <dvarrui@protonmail.com>
2022-08-11 08:02:07 +0100

    [F] tests.

--- lib/asker/loader/embedded_file.rb
+++ lib/asker/loader/embedded_file.rb
@@ -11,47 +11,24 @@ module EmbeddedFile
   # @param localdir (String) Input file base folder
   # @return Hash
   def self.load(value, localdir)
-    # When filename is an URL
-    if value.start_with?('https://') || value.start_with?('http://')
-      if is_image? value
-        html_text = "<img src=\"#{value}\" alt=\"image\" width=\"400\" height=\"300\">"
-      elsif is_audio? value
-        html_text = "<audio src=\"#{value}\" controls></audio>"
-      else
-        html_text = "<b> #{value}: Unkown file type!</b>"
-      end
-      return { text: html_text, file: :none, type: :url }
+    return load_image(value, localdir) if is_image? value
+    return load_audio(value, localdir) if is_audio? value
+    return load_video(value, localdir) if is_video? value
+
+    if is_url? value
+      Logger.verbose Rainbow("[ERROR] Unkown URL type!: #{value}").red.bright
+      exit 1
     end
 
     filepath = File.join(localdir, value)
     unless File.exist?(filepath)
-      # When filename is unkown!
-      Logger.verbose Rainbow("[ERROR] Unknown file! #{filepath}").red.bright
+      Logger.verbose Rainbow("[ERROR] File does not exist!: #{filepath}").red.bright
+      # return { text: "URI error", file: :none, type: :unkown }
       exit 1
     end
-    if is_audio? filepath
-      text = '<audio controls><source src="@@PLUGINFILE@@/' + File.basename(filepath) \
-             + '">Your browser does not support the audio tag.</audio>'
-      data = '<file name="' + File.basename(filepath) + '" path="/" encoding="base64">' \
-             + Base64.strict_encode64(File.open(filepath, 'rb').read) + '</file>'
-      return { text: text, file: data, type: :audio }
-    elsif is_image? filepath
-      text = '<img src="@@PLUGINFILE@@/' + File.basename(filepath) \
-             + '" alt="imagen" class="img-responsive atto_image_button_text-bottom">'
-      data = '<file name="' + File.basename(filepath) + '" path="/" encoding="base64">' \
-             + Base64.strict_encode64(File.open(filepath, 'rb').read) + '</file>'
-      return { text: text, file: data, type: :image }
-    elsif is_video? filepath
-      text = '<video controls><source src="@@PLUGINFILE@@/' + File.basename(filepath) \
-             + '">Your browser does not support the video tag.</video>'
-      data = '<file name="' + File.basename(filepath) + '" path="/" encoding="base64">' \
-             + Base64.strict_encode64(File.open(filepath, 'rb').read) + '</file>'
-      return { text: text, file: data, type: :video }
-    end
-    # Suposse that filename is TXT file
-    return { text: "<pre>#{File.read(filepath)}</pre>", file: :none } if File.exist?(filepath)
 
-    { text: :error, file: :none}
+    # Suposse that filename is TEXT file
+    return { text: "<pre>#{File.read(filepath)}</pre>", file: :none, type: :text }
   end
 
   def self.is_audio?(filename)
@@ -72,13 +49,41 @@ module EmbeddedFile
     false
   end
 
-  # TODO: convertir load a load_image, load_audio y load_video
+  def self.is_url?(value)
+    return true if value.start_with?('https://') || value.start_with?('http://')
+    false
+  end
+
+  def self.load_audio(value, localdir)
+    output = { text: :error, file: :none, type: :audio}
+
+    if is_url? value
+      output[:text] = "<audio src=\"#{value}\" controls></audio>"
+      output[:file] = :none
+      output[:type] = :url
+      return output
+    end
+
+    filepath = File.join(localdir, value)
+    unless File.exist?(filepath)
+      Logger.verbose Rainbow("[ERROR] Audio file no exists!: #{filepath}").red.bright
+      exit 1
+    end
+    output[:text] = '<audio controls><source src="@@PLUGINFILE@@/' + File.basename(filepath) \
+                    + '">Your browser does not support the audio tag.</audio>'
+    output[:file] = '<file name="' + File.basename(filepath) \
+                    + '" path="/" encoding="base64">' \
+                    + Base64.strict_encode64(File.open(filepath, 'rb').read) + '</file>'
+    output[:type] = :audio
+    output
+  end
+
   def self.load_image(value, localdir)
     output = { text: :error, file: :none, type: :image}
 
-    if value.start_with?('https://') || value.start_with?('http://')
+    if is_url? value
       output[:text] = "<img src=\"#{value}\" alt=\"image\" width=\"400\" height=\"300\">"
-      output[:file] = ''
+      output[:file] = :none
       output[:type] = :url
       return output
     end
@@ -97,4 +102,28 @@ module EmbeddedFile
     output
   end
 
+  def self.load_video(value, localdir)
+    output = { text: :error, file: :none, type: :video}
+
+    if is_url? value
+      output[:text] = "<video controls width=\"400\" height=\"300\">" \
+                      + "<source src=\"#{value}\"/></video>"
+      output[:file] = :none
+      output[:type] = :url
+      return output
+    end
+
+    filepath = File.join(localdir, value)
+    unless File.exist?(filepath)
+      Logger.verbose Rainbow("[ERROR] Unknown file! #{filepath}").red.bright
+      exit 1
+    end
+    output[:text] = '<video controls><source src="@@PLUGINFILE@@/' + File.basename(filepath) \
+                    + '"/>Your browser does not support the video tag.</video>'
+    output[:file] = '<file name="' + File.basename(filepath) \
+                    + '" path="/" encoding="base64">' \
+                    + Base64.strict_encode64(File.open(filepath, 'rb').read) + '</file>'
+    output[:type] = :video
+    output
+  end
 end
