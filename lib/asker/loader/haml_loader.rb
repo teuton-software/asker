d57f7b4790d0d53b750e2553128b792de6d62920
David Vargas <dvarrui@protonmail.com>
2022-12-23 11:01:55 +0000

    [F] update haml 5.1 to haml 6.1 gem.

--- lib/asker/loader/haml_loader.rb
+++ lib/asker/loader/haml_loader.rb
@@ -1,19 +1,17 @@
-# frozen_string_literal: true
-
 require 'haml'
 
-# HAML file loader
 module HamlLoader
   def self.load(filename)
     template = File.read(filename)
     begin
-      haml_engine = Haml::Engine.new(template)
-      return haml_engine.render
+      # INFO <haml 5.1>
+      #   haml_engine = Haml::Engine.new(template)
+      #   return haml_engine.render
+      return Haml::Template.new { template }.render
     rescue StandardError => e
       puts "[ERROR] HamlLoader: Can't load <#{filename}> file!"
       puts "  => #{e}"
       exit 0
     end
   end
-
 end
