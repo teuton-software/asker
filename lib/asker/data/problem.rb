c948717f8831e07c36941ca776125c3d4164bf50
dvarrui <dvarrui@protonmail.com>
2023-05-27 18:43:40 +0100

    U New class problema data

--- lib/asker/data/problem.rb
+++ lib/asker/data/problem.rb
@@ -1,2 +1,27 @@
 class Problem
+  attr_accessor :varnames
+  attr_accessor :cases
+  attr_accessor :description
+  attr_accessor :steps
+
+  def initialize
+    @varnames = []
+    @cases = []
+    @description = ""
+    @steps = []
+  end
+
+  class Step
+    attr_accessor :text
+    attr_accessor :formula
+    attr_accessor :varname
+    attr_accessor :feedback
+
+    def initialize(varnames)
+      @varnames = varnames
+      @text = ""
+      @formula = ""
+      @varname = ""
+    end
+  end
 end
