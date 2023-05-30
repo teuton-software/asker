d00d7d2d543d0c22ddf59973a8fc1e595ed5cd72
dvarrui <dvarrui@protonmail.com>
2023-05-30 21:24:13 +0100

    U Move start code from main module to asker/start class

--- lib/asker/start.rb
+++ lib/asker/start.rb
@@ -1,40 +1,21 @@
-require "rainbow"
+require_relative "application"
+require_relative "logger"
+require_relative "displayer/concept_displayer"
+require_relative "displayer/stats_displayer"
+require_relative "exporter/output_file_exporter"
+require_relative "loader/project_loader"
+require_relative "loader/input_loader"
 
-require_relative "asker/check_input"
-require_relative "asker/logger"
-require_relative "asker/skeleton"
-require_relative "asker/start"
-
-require_relative 'asker/displayer/concept_displayer'
-require_relative 'asker/displayer/stats_displayer'
-require_relative 'asker/exporter/output_file_exporter'
-require_relative 'asker/loader/project_loader'
-require_relative 'asker/loader/input_loader'
-
-class Asker
-  def self.init
-    Skeleton.create_configuration
-  end
-
-  def self.create_input(dirpath)
-    Skeleton.create_input(dirpath)
-  end
-
-  def self.check(filepath)
-    CheckInput.new.check(filepath)
-  end
-
-  def self.start(filepath)
-    Start.new.call(filepath)
-  end
-
-  def self.start2(filepath)
+class Start
+  def call(filepath)
     project_data, data = load_input(filepath)
     ConceptDisplayer.show(data[:concepts])
     create_output(project_data, data)
   end
 
-  private_class_method def self.load_input(args)
+  private
+
+  def load_input(args)
     init_project_data
     project_data = ProjectLoader.load(args)
     init_logger(project_data)
@@ -45,7 +26,7 @@ class Asker
     [project_data, data]
   end
 
-  private_class_method def self.init_project_data()
+  def init_project_data()
     project_data = ProjectData.instance
     outputdir = Application.instance.config['output']['folder']
     project_data.set(:outputdir, outputdir)
@@ -54,7 +35,7 @@ class Asker
     project_data.set(:weights, formula_weights)
   end
 
-  private_class_method def self.init_logger(project)
+  def init_logger(project)
     Logger.create(project.get(:logpath))
     Logger.instance.set_verbose(Application.instance.config['verbose'])
     # Logger.verboseln '[INFO] Project open'
@@ -62,7 +43,7 @@ class Asker
     # Logger.verboseln '   └── process_file = ' + Rainbow(project.get(:process_file)).bright
   end
 
-  private_class_method def self.create_output(project, data)
+  def create_output(project, data)
     OutputFileExporter.export(data, project)
     StatsDisplayer.show(data)
     Logger.close
