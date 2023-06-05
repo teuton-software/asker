b2715400cbfae0a37ec855fa7c5738a92bdca0a7
dvarrui <dvarrui@protonmail.com>
2023-06-05 18:46:16 +0100

    N test examples

---
+++ test/examples_test.rb
@@ -0,0 +1,19 @@
+require "test/unit"
+
+class ExamplesTest < Test::Unit::TestCase
+  def setup
+    @filepaths = Dir.glob(File.join("docs", "examples", "**", "*haml"))
+  end
+
+  def test_asker_check_examples
+    @filepaths.each do |filepath|
+      assert_equal true, system("asker check #{filepath} >/dev/null")
+    end
+  end
+
+  def test_asker_file_examples
+    @filepaths.each do |filepath|
+      assert_equal true, system("asker file #{filepath} >/dev/null")
+    end
+  end
+end
