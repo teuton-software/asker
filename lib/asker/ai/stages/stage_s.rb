6810378c1191810fc9fafadc44f56600583cae22
David Vargas <dvarrui@protonmail.com>
2023-06-12 07:55:00 +0100

    U Add ordering type questions to Concept sequence

--- lib/asker/ai/stages/stage_s.rb
+++ lib/asker/ai/stages/stage_s.rb
@@ -3,19 +3,14 @@
 require_relative "base_stage"
 require_relative "../question"
 
-# process_sequence
 class StageS < BaseStage
   # process_sequence
-  # rubocop:disable Metrics/MethodLength
-  # rubocop:disable Metrics/AbcSize
-  # rubocop:disable Metrics/CyclomaticComplexity
-  # rubocop:disable Metrics/PerceivedComplexity
   def run(table, list1, _list2)
     questions = []
     return questions unless table.fields.count == 1 && table.sequence? && table.sequence[0] != ""
 
     lang = concept.lang
-    # Question type <d3sequence>: items are part of a sequence
+    # Question: items are part of a sequence
     if list1.count > 3
       a = 0..(list1.count - 4)
       a.each_entry do |i|
@@ -29,9 +24,15 @@ class StageS < BaseStage
         q.matching << ["", lang.text_for(:error)]
         questions << q
       end
+
+      q = Question.new(:ordering)
+      q.name = "#{name}-#{num}-s1sequence-#{table.name}"
+      q.text = random_image_for(name) + lang.text_for(:s1, name, table.fields[0].capitalize, table.sequence[0])
+      list1.each { |item| q.ordering << item[:data][0] }
+      questions << q
     end
 
-    # Question type <d4sequence>: items are part of a reverse sequence
+    # Question: items are part of a reverse sequence
     if list1.count > 3 && table.sequence.size > 1
       a = 0..(list1.count - 4)
       a.each_entry do |i|
@@ -46,12 +47,14 @@ class StageS < BaseStage
         q.matching << ["", lang.text_for(:error)]
         questions << q
       end
+
+      q = Question.new(:ordering)
+      q.name = "#{name}-#{num}-s2sequence-#{table.name}"
+      q.text = random_image_for(name) + lang.text_for(:s1, name, table.fields[0].capitalize, table.sequence[1])
+      list1.reverse.to_a.each { |item| q.ordering << item[:data][0] }
+      questions << q
     end
 
     questions
   end
-  # rubocop:enable Metrics/MethodLength
-  # rubocop:enable Metrics/AbcSize
-  # rubocop:enable Metrics/CyclomaticComplexity
-  # rubocop:enable Metrics/PerceivedComplexity
 end
