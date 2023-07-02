f9e3945d7f4b27edb72c19ac06ea6ba26a720033
dvarrui <dvarrui@protonmail.com>
2023-07-02 11:38:39 +0100

    [U] show problem stats

--- lib/asker/displayer/problem_displayer.rb
+++ lib/asker/displayer/problem_displayer.rb
@@ -8,9 +8,9 @@ class ProblemDisplayer
   def call(problems)
     return if problems.nil? || problems.size.zero?
 
-    total_p = total_q = total_e = 0
+    total_p = total_q = total_e = total_a = total_s = 0
     my_screen_table = Terminal::Table.new do |st|
-      st << %w[Problem Desc Questions Entries xFactor]
+      st << %w[Problem Desc Questions Entries xFactor a s]
       st << :separator
     end
 
@@ -23,14 +23,19 @@ class ProblemDisplayer
         e += 1 if !ask[:answer].nil?
       end
 
+      desc = Rainbow(problem.desc[0, 24]).green
       q = problem.questions.size
       factor = "Unknown"
       factor = (q.to_f / e).round(2).to_s unless e.zero?
-      desc = Rainbow(problem.desc[0, 24]).green
-      my_screen_table.add_row [problem.name, desc, q, e, factor]
+      a = problem.stats[:answer]
+      s = problem.stats[:steps]
+
+      my_screen_table.add_row [problem.name, desc, q, e, factor, a, s]
       total_p += 1
       total_q += q
       total_e += e
+      total_a += a
+      total_s += s
     end
     return unless total_p.positive?
 
@@ -38,7 +43,9 @@ class ProblemDisplayer
     my_screen_table.add_row [Rainbow("TOTAL = #{total_p}").bright, "",
       Rainbow(total_q.to_s).bright,
       Rainbow(total_e.to_s).bright,
-      Rainbow((total_q / total_e.to_f).round(2)).bright]
+      Rainbow((total_q / total_e.to_f).round(2)).bright,
+      Rainbow(total_a.to_s).bright,
+      Rainbow(total_s.to_s).bright]
     Logger.verboseln Rainbow("\n[INFO] Showing PROBLEMs statistics").white
     Logger.verboseln my_screen_table.to_s
   end
